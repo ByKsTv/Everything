836366df0eb24e31d79756677f23f603f44169d1
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-04-30 22:04:18 +0300

    a

--- Windows/Windows10_Step5.ps1
+++ Windows/Windows10_Step5.ps1
@@ -1,9 +1,14 @@
+Add-Type -AssemblyName System.Windows.Forms
 Unregister-ScheduledTask -TaskName Windows10_Step5 -Confirm:$false
 Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Firefox/Arkenfox.ps1 | Invoke-Expression
 Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Windows10_Step5_Settings.ps1 | Invoke-Expression
 Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Windows10_Step5_Network.ps1 | Invoke-Expression
 Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Firefox/Extensions.ps1 | Invoke-Expression
-Add-Type -AssemblyName System.Windows.Forms
+$MPVAnswer = [System.Windows.Forms.MessageBox]::Show('Install mpv?' , 'mpv' , 4, 32)
+if ($MPVAnswer -eq 'Yes') {
+	Write-Host 'mpv > Install' -ForegroundColor green -BackgroundColor black
+	Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/mpv/Download_Install_AutoUpdate.ps1 | Invoke-Expression
+}
 $EdgeUninstallAnswer = [System.Windows.Forms.MessageBox]::Show('Uninstall Edge?' , 'Uninstall Edge' , 4, 32)
 if ($EdgeUninstallAnswer -eq 'Yes') {
 	Write-Host 'Microsoft Edge > Uninstall' -ForegroundColor green -BackgroundColor black
