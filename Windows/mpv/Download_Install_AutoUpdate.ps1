6bd002da5e532b7285b50c2fd1558bf9ff9ec054
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-05-03 02:25:35 +0300

    a

--- Windows/mpv/Download_Install_AutoUpdate.ps1
+++ Windows/mpv/Download_Install_AutoUpdate.ps1
@@ -48,6 +48,7 @@ Start-Sleep 1
 [System.Windows.Forms.SendKeys]::SendWait('{1}')
 While (!(Test-Path $env:ProgramFiles\mpv\installer\mpv-install.bat -ErrorAction SilentlyContinue)) {
 }
+Start-Sleep 1
 Write-Host 'mpv > Install' -ForegroundColor green -BackgroundColor black
 Start-Process -FilePath $env:ProgramFiles\mpv\installer\mpv-install.bat -ArgumentList /u
 Write-Host 'mpv > mpv.conf' -ForegroundColor green -BackgroundColor black
