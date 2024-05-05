64d7036f85e510a769979c526fc68f3a415bebbd
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-05-05 18:05:30 +0300

    a

--- Windows/Step4.ps1
+++ Windows/Step4.ps1
@@ -6,6 +6,10 @@ Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windo
 Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Network.ps1 | Invoke-Expression
 Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Firefox/Extensions.ps1 | Invoke-Expression
 Add-Type -AssemblyName System.Windows.Forms
+$PythonAnswer = [System.Windows.Forms.MessageBox]::Show('Install Python?' , 'Python' , 4, 32)
+if ($PythonAnswer -eq 'Yes') {
+    Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/mpv/Download.ps1 | Invoke-Expression
+}
 $MPVAnswer = [System.Windows.Forms.MessageBox]::Show('Install mpv?' , 'mpv' , 4, 32)
 if ($MPVAnswer -eq 'Yes') {
     Write-Host 'mpv > Install' -ForegroundColor green -BackgroundColor black
