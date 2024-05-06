ef968a4eb7e507916a2f38b7beeaf876e4762ac0
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-05-06 21:01:00 +0300

    a

---
+++ Windows/Discord/README.md
@@ -0,0 +1,13 @@
+# Information
+
+> [!NOTE]
+> Everything about Discord.
+
+## Discord Setup - Auto Install
+
+1. PowerShell (Admin):
+
+```powershell
+Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Discord/Download.ps1 | Invoke-Expression
+
+```
--- Windows/Step4.ps1
+++ Windows/Step4.ps1
@@ -9,11 +9,10 @@ Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windo
 Add-Type -AssemblyName System.Windows.Forms
 $PythonAnswer = [System.Windows.Forms.MessageBox]::Show('Install Python?' , 'Python' , 4, 32)
 if ($PythonAnswer -eq 'Yes') {
-    Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/mpv/Download.ps1 | Invoke-Expression
+    Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Python/Download.ps1 | Invoke-Expression
 }
 $MPVAnswer = [System.Windows.Forms.MessageBox]::Show('Install mpv?' , 'mpv' , 4, 32)
 if ($MPVAnswer -eq 'Yes') {
-    Write-Host 'mpv > Install' -ForegroundColor green -BackgroundColor black
     Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/mpv/Download.ps1 | Invoke-Expression
 }
 $OfficeAnswer = [System.Windows.Forms.MessageBox]::Show('Install Office?' , 'Office' , 4, 32)
@@ -23,6 +22,10 @@ if ($OfficeAnswer -eq 'Yes') {
 $qBittorrentAnswer = [System.Windows.Forms.MessageBox]::Show('Install qBittorrent?' , 'qBittorrent' , 4, 32)
 if ($qBittorrentAnswer -eq 'Yes') {
     Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/qBittorrent/Download.ps1 | Invoke-Expression
+}
+$DiscordAnswer = [System.Windows.Forms.MessageBox]::Show('Install Discord?' , 'Discord' , 4, 32)
+if ($DiscordAnswer -eq 'Yes') {
+    Invoke-RestMethod https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Discord/Download.ps1 | Invoke-Expression
 }
 $EdgeUninstallAnswer = [System.Windows.Forms.MessageBox]::Show('Uninstall Edge?
 
