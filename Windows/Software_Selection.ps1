0d9a5eccb0b9d67e59a946668ae6ea926756f6c6
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-05-12 03:02:56 +0300

    software selection: detect if software already installed

--- Windows/Software_Selection.ps1
+++ Windows/Software_Selection.ps1
@@ -16,12 +16,22 @@ $Check7Zip.Text = '7-Zip'
 $Check7Zip.Checked = $false
 $Form.Controls.Add($Check7Zip)
 
+if ((Test-Path -Path $env:ProgramFiles\7-Zip)) {
+    $Check7Zip.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $Check7Zip.Text += ' (Installed)'
+}
+
 $CheckAnyDesk = New-Object System.Windows.Forms.checkbox
 $CheckAnyDesk.Location = New-Object System.Drawing.Size(30, 50)
 $CheckAnyDesk.Size = New-Object System.Drawing.Size(250, 20)
 $CheckAnyDesk.Text = 'AnyDesk'
 $CheckAnyDesk.Checked = $false
 $Form.Controls.Add($CheckAnyDesk)
+
+if ((Test-Path -Path ${env:ProgramFiles(x86)}\AnyDesk)) {
+    $CheckAnyDesk.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckAnyDesk.Text += ' (Installed)'
+}
 
 $CheckBattleNet = New-Object System.Windows.Forms.checkbox
 $CheckBattleNet.Location = New-Object System.Drawing.Size(30, 70)
@@ -30,12 +40,23 @@ $CheckBattleNet.Text = 'Battle.net'
 $CheckBattleNet.Checked = $false
 $Form.Controls.Add($CheckBattleNet)
 
+if ((Test-Path -Path ${env:ProgramFiles(x86)}\Battle.net)) {
+    $CheckBattleNet.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckBattleNet.Text += ' (Installed)'
+
+}
+
 $CheckCrystalDiskInfo = New-Object System.Windows.Forms.checkbox
 $CheckCrystalDiskInfo.Location = New-Object System.Drawing.Size(30, 90)
 $CheckCrystalDiskInfo.Size = New-Object System.Drawing.Size(250, 20)
 $CheckCrystalDiskInfo.Text = 'CrystalDiskInfo'
 $CheckCrystalDiskInfo.Checked = $false
 $Form.Controls.Add($CheckCrystalDiskInfo)
+
+if ((Test-Path -Path $env:ProgramFiles\CrystalDiskInfo)) {
+    $CheckCrystalDiskInfo.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckCrystalDiskInfo.Text += ' (Installed)'
+}
 
 $CheckCrystalDiskMark = New-Object System.Windows.Forms.checkbox
 $CheckCrystalDiskMark.Location = New-Object System.Drawing.Size(30, 110)
@@ -44,12 +65,22 @@ $CheckCrystalDiskMark.Text = 'CrystalDiskMark'
 $CheckCrystalDiskMark.Checked = $false
 $Form.Controls.Add($CheckCrystalDiskMark)
 
+if ((Test-Path -Path $env:ProgramFiles\CrystalDiskMark8)) {
+    $CheckCrystalDiskMark.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckCrystalDiskMark.Text += ' (Installed)'
+}
+
 $CheckDiscord = New-Object System.Windows.Forms.checkbox
 $CheckDiscord.Location = New-Object System.Drawing.Size(30, 130)
 $CheckDiscord.Size = New-Object System.Drawing.Size(250, 20)
 $CheckDiscord.Text = 'Discord'
 $CheckDiscord.Checked = $false
 $Form.Controls.Add($CheckDiscord)
+
+if ((Test-Path -Path $env:LOCALAPPDATA\Discord)) {
+    $CheckDiscord.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckDiscord.Text += ' (Installed)'
+}
 
 $CheckDisplayDriverUninstaller = New-Object System.Windows.Forms.checkbox
 $CheckDisplayDriverUninstaller.Location = New-Object System.Drawing.Size(30, 150)
@@ -58,12 +89,23 @@ $CheckDisplayDriverUninstaller.Text = 'Display Driver Uninstaller'
 $CheckDisplayDriverUninstaller.Checked = $false
 $Form.Controls.Add($CheckDisplayDriverUninstaller)
 
+if ((Test-Path -Path "${env:ProgramFiles(x86)}\Display Driver Uninstaller")) {
+    $CheckDisplayDriverUninstaller.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckDisplayDriverUninstaller.Text += ' (Installed)'
+}
+
 $CheckHyperXNGENUITY = New-Object System.Windows.Forms.checkbox
 $CheckHyperXNGENUITY.Location = New-Object System.Drawing.Size(30, 170)
 $CheckHyperXNGENUITY.Size = New-Object System.Drawing.Size(250, 20)
 $CheckHyperXNGENUITY.Text = 'HyperX NGENUITY'
 $CheckHyperXNGENUITY.Checked = $false
 $Form.Controls.Add($CheckHyperXNGENUITY)
+
+if ((Get-ChildItem $env:ProgramFiles\WindowsApps) -like '*NGENUITY*') {
+    $CheckHyperXNGENUITY.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckHyperXNGENUITY.Text += ' (Installed)'
+}
+
 
 $CheckJellyfin = New-Object System.Windows.Forms.checkbox
 $CheckJellyfin.Location = New-Object System.Drawing.Size(30, 190)
@@ -72,6 +114,11 @@ $CheckJellyfin.Text = 'Jellyfin'
 $CheckJellyfin.Checked = $false
 $Form.Controls.Add($CheckJellyfin)
 
+if ((Test-Path -Path $env:ProgramFiles\Jellyfin)) {
+    $CheckJellyfin.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckJellyfin.Text += ' (Installed)'
+}
+
 $CheckLogitechGHUB = New-Object System.Windows.Forms.checkbox
 $CheckLogitechGHUB.Location = New-Object System.Drawing.Size(30, 210)
 $CheckLogitechGHUB.Size = New-Object System.Drawing.Size(250, 20)
@@ -79,12 +126,22 @@ $CheckLogitechGHUB.Text = 'Logitech G HUB'
 $CheckLogitechGHUB.Checked = $false
 $Form.Controls.Add($CheckLogitechGHUB)
 
+if ((Test-Path -Path $env:ProgramFiles\LGHUB)) {
+    $CheckLogitechGHUB.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckLogitechGHUB.Text += ' (Installed)'
+}
+
 $CheckMediaInfo = New-Object System.Windows.Forms.checkbox
 $CheckMediaInfo.Location = New-Object System.Drawing.Size(30, 230)
 $CheckMediaInfo.Size = New-Object System.Drawing.Size(250, 20)
 $CheckMediaInfo.Text = 'MediaInfo'
 $CheckMediaInfo.Checked = $false
 $Form.Controls.Add($CheckMediaInfo)
+
+if ((Test-Path -Path $env:ProgramFiles\MediaInfo)) {
+    $CheckMediaInfo.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckMediaInfo.Text += ' (Installed)'
+}
 
 $CheckNVCleanstall = New-Object System.Windows.Forms.checkbox
 $CheckNVCleanstall.Location = New-Object System.Drawing.Size(30, 250)
@@ -98,6 +155,12 @@ If ((Get-WmiObject Win32_VideoController).Name -like '*NVIDIA*') {
 }
 If ((Get-WmiObject Win32_VideoController).Name -notlike '*NVIDIA*') {
     $CheckNVCleanstall.Text += ' (Incompatible GPU)'
+    $CheckNVCleanstall.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#ff0000')
+}
+
+if ((Test-Path -Path $env:ProgramFiles\NVCleanstall)) {
+    $CheckNVCleanstall.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckNVCleanstall.Text += ' (Installed)'
 }
 
 $CheckNordVPN = New-Object System.Windows.Forms.checkbox
@@ -107,12 +170,22 @@ $CheckNordVPN.Text = 'NordVPN'
 $CheckNordVPN.Checked = $false
 $Form.Controls.Add($CheckNordVPN)
 
+if ((Test-Path -Path $env:ProgramFiles\NordVPN)) {
+    $CheckNordVPN.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckNordVPN.Text += ' (Installed)'
+}
+
 $CheckNotepadPlusPlus = New-Object System.Windows.Forms.checkbox
 $CheckNotepadPlusPlus.Location = New-Object System.Drawing.Size(30, 290)
 $CheckNotepadPlusPlus.Size = New-Object System.Drawing.Size(250, 20)
 $CheckNotepadPlusPlus.Text = 'Notepad++'
 $CheckNotepadPlusPlus.Checked = $false
 $Form.Controls.Add($CheckNotepadPlusPlus)
+
+if ((Test-Path -Path $env:ProgramFiles\Notepad++)) {
+    $CheckNotepadPlusPlus.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckNotepadPlusPlus.Text += ' (Installed)'
+}
 
 $CheckOffice = New-Object System.Windows.Forms.checkbox
 $CheckOffice.Location = New-Object System.Drawing.Size(30, 310)
@@ -121,6 +194,11 @@ $CheckOffice.Text = 'Office'
 $CheckOffice.Checked = $false
 $Form.Controls.Add($CheckOffice)
 
+if ((Test-Path -Path "$env:ProgramFiles\Microsoft Office")) {
+    $CheckOffice.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckOffice.Text += ' (Installed)'
+}
+
 $CheckPlex = New-Object System.Windows.Forms.checkbox
 $CheckPlex.Location = New-Object System.Drawing.Size(30, 330)
 $CheckPlex.Size = New-Object System.Drawing.Size(250, 20)
@@ -128,61 +206,118 @@ $CheckPlex.Text = 'Plex'
 $CheckPlex.Checked = $false
 $Form.Controls.Add($CheckPlex)
 
+if ((Test-Path -Path $env:ProgramFiles\Plex)) {
+    $CheckPlex.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckPlex.Text += ' (Installed)'
+}
+
+$CheckPuTTY = New-Object System.Windows.Forms.checkbox
+$CheckPuTTY.Location = New-Object System.Drawing.Size(30, 350)
+$CheckPuTTY.Size = New-Object System.Drawing.Size(250, 20)
+$CheckPuTTY.Text = 'PuTTY'
+$CheckPuTTY.Checked = $false
+$Form.Controls.Add($CheckPuTTY)
+
+if ((Test-Path -Path $env:ProgramFiles\PuTTY)) {
+    $CheckPuTTY.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckPuTTY.Text += ' (Installed)'
+}
+
 $CheckPython = New-Object System.Windows.Forms.checkbox
-$CheckPython.Location = New-Object System.Drawing.Size(30, 350)
+$CheckPython.Location = New-Object System.Drawing.Size(30, 370)
 $CheckPython.Size = New-Object System.Drawing.Size(250, 20)
 $CheckPython.Text = 'Python'
 $CheckPython.Checked = $false
 $Form.Controls.Add($CheckPython)
 
+if ((Test-Path -Path $env:LOCALAPPDATA\Programs\Python)) {
+    $CheckPython.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckPython.Text += ' (Installed)'
+}
+
 $CheckRazerSynapse = New-Object System.Windows.Forms.checkbox
-$CheckRazerSynapse.Location = New-Object System.Drawing.Size(30, 370)
+$CheckRazerSynapse.Location = New-Object System.Drawing.Size(30, 390)
 $CheckRazerSynapse.Size = New-Object System.Drawing.Size(250, 20)
 $CheckRazerSynapse.Text = 'Razer Synapse'
 $CheckRazerSynapse.Checked = $false
 $Form.Controls.Add($CheckRazerSynapse)
 
+if ((Test-Path -Path ${env:ProgramFiles(x86)}\Razer)) {
+    $CheckRazerSynapse.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckRazerSynapse.Text += ' (Installed)'
+}
+
 $CheckSteam = New-Object System.Windows.Forms.checkbox
-$CheckSteam.Location = New-Object System.Drawing.Size(30, 390)
+$CheckSteam.Location = New-Object System.Drawing.Size(30, 410)
 $CheckSteam.Size = New-Object System.Drawing.Size(250, 20)
 $CheckSteam.Text = 'Steam'
 $CheckSteam.Checked = $false
 $Form.Controls.Add($CheckSteam)
 
+if ((Test-Path -Path ${env:ProgramFiles(x86)}\Steam)) {
+    $CheckSteam.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckSteam.Text += ' (Installed)'
+}
+
 $CheckTelegram = New-Object System.Windows.Forms.checkbox
-$CheckTelegram.Location = New-Object System.Drawing.Size(30, 410)
+$CheckTelegram.Location = New-Object System.Drawing.Size(30, 430)
 $CheckTelegram.Size = New-Object System.Drawing.Size(250, 20)
 $CheckTelegram.Text = 'Telegram'
 $CheckTelegram.Checked = $false
 $Form.Controls.Add($CheckTelegram)
 
+if ((Test-Path -Path "$env:APPDATA\Telegram Desktop")) {
+    $CheckTelegram.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckTelegram.Text += ' (Installed)'
+}
+
 $CheckVisualStudioCode = New-Object System.Windows.Forms.checkbox
-$CheckVisualStudioCode.Location = New-Object System.Drawing.Size(30, 430)
+$CheckVisualStudioCode.Location = New-Object System.Drawing.Size(30, 450)
 $CheckVisualStudioCode.Size = New-Object System.Drawing.Size(250, 20)
 $CheckVisualStudioCode.Text = 'Visual Studio Code'
 $CheckVisualStudioCode.Checked = $false
 $Form.Controls.Add($CheckVisualStudioCode)
 
+if ((Test-Path -Path "$env:LOCALAPPDATA\Programs\Microsoft VS Code")) {
+    $CheckVisualStudioCode.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckVisualStudioCode.Text += ' (Installed)'
+}
+
 $Checkmpv = New-Object System.Windows.Forms.checkbox
-$Checkmpv.Location = New-Object System.Drawing.Size(30, 450)
+$Checkmpv.Location = New-Object System.Drawing.Size(30, 470)
 $Checkmpv.Size = New-Object System.Drawing.Size(250, 20)
 $Checkmpv.Text = 'mpv'
 $Checkmpv.Checked = $false
 $Form.Controls.Add($Checkmpv)
 
+if ((Test-Path -Path $env:ProgramFiles\mpv)) {
+    $Checkmpv.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $Checkmpv.Text += ' (Installed)'
+}
+
 $CheckqBittorrent = New-Object System.Windows.Forms.checkbox
-$CheckqBittorrent.Location = New-Object System.Drawing.Size(30, 470)
+$CheckqBittorrent.Location = New-Object System.Drawing.Size(30, 490)
 $CheckqBittorrent.Size = New-Object System.Drawing.Size(250, 20)
 $CheckqBittorrent.Text = 'qBittorrent'
 $CheckqBittorrent.Checked = $false
 $Form.Controls.Add($CheckqBittorrent)
 
+if ((Test-Path -Path $env:ProgramFiles\qBittorrent)) {
+    $CheckqBittorrent.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckqBittorrent.Text += ' (Installed)'
+}
+
 $CheckUninstallEdge = New-Object System.Windows.Forms.checkbox
-$CheckUninstallEdge.Location = New-Object System.Drawing.Size(30, 490)
+$CheckUninstallEdge.Location = New-Object System.Drawing.Size(30, 510)
 $CheckUninstallEdge.Size = New-Object System.Drawing.Size(250, 20)
 $CheckUninstallEdge.Text = 'Uninstall Edge (Not Recommended)'
 $CheckUninstallEdge.Checked = $false
 $Form.Controls.Add($CheckUninstallEdge)
+
+if (!(Test-Path -Path ${env:ProgramFiles(x86)}\Microsoft\Edge\Application\msedge.exe)) {
+    $CheckUninstallEdge.ForeColor = [System.Drawing.ColorTranslator]::FromHtml('#009600')
+    $CheckUninstallEdge.Text += ' (Uninstalled)'
+}
 
 $OKButton = New-Object System.Windows.Forms.Button
 $OKButton.Location = New-Object System.Drawing.Size(130, 600)
@@ -254,6 +389,9 @@ $OKButton.Add_Click{
     }
     if ($CheckNotepadPlusPlus.Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Notepad_Plus_Plus/Download.ps1')
+    }
+    if ($CheckPuTTY.Checked) {
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/PuTTY/Download.ps1')
     }
     if ($CheckPython.Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Python/Download.ps1')
