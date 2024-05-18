bccc24ac8c8ff2812ca169df64f1347c47915191
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-05-18 15:02:18 +0300

    a

--- Windows/CrystalDiskMark/Download.ps1
+++ Windows/CrystalDiskMark/Download.ps1
@@ -1,7 +1,7 @@
 Write-Host 'CrystalDiskMark: Downloading' -ForegroundColor green -BackgroundColor black
 $CrystalDiskMark = New-Object System.Net.WebClient
 $CrystalDiskMark.Headers.Add('user-agent', 'Wget')
-$CrystalDiskMark.DownloadFile('https://sourceforge.net/projects/CrystalDiskMark/files/latest/download', "$ENV:temp\CrystalDiskMark.exe")
+$CrystalDiskMark.DownloadFile('https://crystalmark.info/redirect.php?product=CrystalDiskMarkInstaller', "$ENV:temp\CrystalDiskMark.exe")
 
 Write-Host 'CrystalDiskMark: Installing' -ForegroundColor green -BackgroundColor black
 Start-Process $ENV:temp\CrystalDiskMark.exe -ArgumentList '/VERYSILENT'
--- Windows/OO_ShutUp10/Download.ps1
+++ Windows/OO_ShutUp10/Download.ps1
@@ -2,8 +2,7 @@ Write-Host 'O&O ShutUp10++: Downloading' -ForegroundColor green -BackgroundColor
 (New-Object System.Net.WebClient).DownloadFile('https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe', "$env:TEMP\OOSU10.exe")
 
 Write-Host 'O&O ShutUp10++: Getting latest recommended settings' -ForegroundColor green -BackgroundColor black
-$OOSU10 = '
-############################################################################
+$OOSU10 = '############################################################################
 # This file was created with O&O ShutUp10++ V1.9.1436
 # and can be imported onto another computer. 
 #
@@ -178,8 +177,7 @@ M012	-
 M013	-
 M014	-
 M023	-
-N001	-
-'
+N001	-'
 
 Write-Host 'O&O ShutUp10++: Writing latest recommended settings' -ForegroundColor green -BackgroundColor black
 New-Item -Path $ENV:temp\OOSU10.cfg -ItemType File -Value $OOSU10 -Force
