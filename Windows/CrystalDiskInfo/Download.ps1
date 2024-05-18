f894c7dcded34b6b7eb0a6465af11e194ac9d0a8
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-05-18 15:09:11 +0300

    a

--- Windows/CrystalDiskInfo/Download.ps1
+++ Windows/CrystalDiskInfo/Download.ps1
@@ -1,7 +1,7 @@
 Write-Host 'CrystalDiskInfo: Downloading' -ForegroundColor green -BackgroundColor black
 $CrystalDiskInfo = New-Object System.Net.WebClient
 $CrystalDiskInfo.Headers.Add('user-agent', 'Wget')
-$CrystalDiskInfo.DownloadFile('https://sourceforge.net/projects/CrystalDiskInfo/files/latest/download', "$ENV:temp\CrystalDiskInfo.exe")
+$CrystalDiskInfo.DownloadFile('https://crystalmark.info/redirect.php?product=CrystalDiskInfoInstaller', "$ENV:temp\CrystalDiskInfo.exe")
 
 Write-Host 'CrystalDiskInfo: Installing' -ForegroundColor green -BackgroundColor black
 Start-Process $ENV:temp\CrystalDiskInfo.exe -ArgumentList '/VERYSILENT'
