21128cf9db57f839e0a2cf1614d2de614de3c33e
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-05-30 13:50:33 +0300

    a

--- Windows/Firefox/user-overrides.js
+++ Windows/Firefox/user-overrides.js
@@ -77,5 +77,8 @@ user_pref("browser.sessionstore.resume_from_crash", false);
 // Disable More from Mozilla
 user_pref("browser.preferences.moreFromMozilla", false);
 
+// Disable Firefox is full screen
+user_pref("full-screen-api.warning.timeout", 0);
+
 // Custom UI
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"save-to-pocket-button\",\"downloads-button\",\"unified-extensions-button\",\"fxa-toolbar-menu-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\"],\"currentVersion\":20,\"newElementCount\":5}");
