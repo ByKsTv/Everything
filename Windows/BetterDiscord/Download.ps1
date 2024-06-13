52d0da257fab1594bb7627e9e10bb1fac593dea9
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-06-13 04:19:52 +0300

    a

--- Windows/BetterDiscord/Download.ps1
+++ Windows/BetterDiscord/Download.ps1
@@ -59,7 +59,7 @@ foreach ($DiscordAppDir in $DiscordAppDirs) {
             Write-Host 'BetterDiscord: Adding DoNotTrack.plugin.js' -ForegroundColor green -BackgroundColor black
             (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/DoNotTrack/DoNotTrack.plugin.js', "$env:APPDATA\BetterDiscord\plugins\DoNotTrack.plugin.js")
 
-            Write-Host 'BetterDiscord: Adding 0PluginLibrary.plugin.js (DoNotTrack)' -ForegroundColor green -BackgroundColor black
+            Write-Host 'BetterDiscord: Adding 0PluginLibrary.plugin.js' -ForegroundColor green -BackgroundColor black
             (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js', "$env:APPDATA\BetterDiscord\plugins\0PluginLibrary.plugin.js")
 
             Write-Host 'BetterDiscord: Adding PluginRepo.plugin.js' -ForegroundColor green -BackgroundColor black
@@ -123,4 +123,54 @@ module.exports = require("./core.asar");
 "@ -Force
         (Get-Content $DiscordIndex).Replace('\', '\\') | Set-Content $DiscordIndex -Force
     }
+}
+
+Write-Host 'BetterDiscord: Updating Themes and Plugins' -ForegroundColor green -BackgroundColor black
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\themes\amoled-cord.theme.css")) {
+    Write-Host 'BetterDiscord: Updating amoled-cord.theme.css' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/LuckFire/amoled-cord/main/clients/amoled-cord.theme.css', "$env:APPDATA\BetterDiscord\themes\amoled-cord.theme.css")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\DoNotTrack.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating DoNotTrack.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/DoNotTrack/DoNotTrack.plugin.js', "$env:APPDATA\BetterDiscord\plugins\DoNotTrack.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\0PluginLibrary.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating 0PluginLibrary.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js', "$env:APPDATA\BetterDiscord\plugins\0PluginLibrary.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\PluginRepo.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating PluginRepo.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/PluginRepo/PluginRepo.plugin.js', "$env:APPDATA\BetterDiscord\plugins\PluginRepo.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\0BDFDB.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating 0BDFDB.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Library/0BDFDB.plugin.js', "$env:APPDATA\BetterDiscord\plugins\0BDFDB.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\CallTimeCounter.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating CallTimeCounter.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/QWERTxD/BetterDiscordPlugins/main/CallTimeCounter/CallTimeCounter.plugin.js', "$env:APPDATA\BetterDiscord\plugins\CallTimeCounter.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\BetterRoleColors.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating BetterRoleColors.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/BetterRoleColors/BetterRoleColors.plugin.js', "$env:APPDATA\BetterDiscord\plugins\BetterRoleColors.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\ThemeRepo.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating ThemeRepo.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/ThemeRepo/ThemeRepo.plugin.js', "$env:APPDATA\BetterDiscord\plugins\ThemeRepo.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\YABDP4Nitro.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating YABDP4Nitro.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/riolubruh/YABDP4Nitro/main/YABDP4Nitro.plugin.js', "$env:APPDATA\BetterDiscord\plugins\YABDP4Nitro.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\RemoveChatButtons.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating RemoveChatButtons.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/BleedingBD/plugin-RemoveChatButtons/main/RemoveChatButtons.plugin.js', "$env:APPDATA\BetterDiscord\plugins\RemoveChatButtons.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\removeTrackingURL.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating removeTrackingURL.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/wotanut/DiscordStuff/main/plugins/dist/removeTrackingURL.plugin.js', "$env:APPDATA\BetterDiscord\plugins\removeTrackingURL.plugin.js")
+}
+if ((Test-Path -Path "$env:APPDATA\BetterDiscord\plugins\UncompressedImages.plugin.js")) {
+    Write-Host 'BetterDiscord: Updating UncompressedImages.plugin.js' -ForegroundColor green -BackgroundColor black
+    (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Knewest/uncompressed-discord-images/main/UncompressedImages.plugin.js', "$env:APPDATA\BetterDiscord\plugins\UncompressedImages.plugin.js")
 }
