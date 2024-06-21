4e7d1323616cc4117ba561f992a8b940fa10027e
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-06-22 02:58:45 +0300

    7zip downloader update

--- Windows/7Zip/Download.ps1
+++ Windows/7Zip/Download.ps1
@@ -16,17 +16,9 @@ if (!($7Zip_Exists)) {
     Register-ScheduledTask @7Zip_Parameters -Force
 }
 
-Write-Host '7-Zip: Getting latest release' -ForegroundColor green -BackgroundColor black
-$dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -UseBasicParsing -Uri 'https://7-zip.org/' | Select-Object -ExpandProperty Links | Where-Object { ($_.outerHTML -match 'Download') -and ($_.href -like 'a/*') -and ($_.href -like '*-x64.exe') } | Select-Object -First 1 | Select-Object -ExpandProperty href)
-$installerPath = Join-Path $env:TEMP (Split-Path $dlurl -Leaf)
-
 Write-Host '7-Zip: Checking if updated' -ForegroundColor green -BackgroundColor black
 $7ZipInstalledVer = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\7-Zip' -ErrorAction SilentlyContinue).DisplayVersion
-$7zipRss = 'https://sourceforge.net/projects/sevenzip/rss?path=/7-Zip'
-$webClient = New-Object System.Net.WebClient
-$rss = [xml]($webClient.DownloadString($7zipRss))
-$fileInfo = $rss.rss.channel.item | Where-Object { $PSItem.link -Match '\.exe' } | Select-Object -First 1
-$7ZipLatestVer = ($fileInfo.title.'#cdata-section' -Split '/')[2]    
+$7ZipLatestVer = (Invoke-RestMethod -Method GET -Uri 'https://api.github.com/repos/ip7z/7zip/releases/latest').tag_name
 
 if ($7ZipInstalledVer -match $7ZipLatestVer) {
     Write-Host '7-Zip: Latest Version Is Already Installed' -ForegroundColor green -BackgroundColor black
@@ -34,8 +26,8 @@ if ($7ZipInstalledVer -match $7ZipLatestVer) {
 
 elseif ($7ZipInstalledVer -notmatch $7ZipLatestVer ) {
     Write-Host '7-Zip: Downloading' -ForegroundColor green -BackgroundColor black
-    (New-Object System.Net.WebClient).DownloadFile($dlurl, $installerPath)
+    (New-Object System.Net.WebClient).DownloadFile(((Invoke-RestMethod -Method GET -Uri 'https://api.github.com/repos/ip7z/7zip/releases/latest').assets | Where-Object name -Like '*-x64.msi*').browser_download_url, "$env:TEMP\7zip-x64.msi")
     
     Write-Host '7-Zip: Installing' -ForegroundColor green -BackgroundColor black
-    Start-Process -FilePath $installerPath -ArgumentList '/S' -Wait
+    Start-Process -FilePath "$env:TEMP\7zip-x64.msi" -ArgumentList '/quiet /norestart' -Wait
 }
