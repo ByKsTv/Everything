aade68aa907cdd6d43108e4fa860bdac841ac6d1
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-06-25 15:41:20 +0300

    hidecursor.lua: reformat

--- Windows/mpv/scripts/hidecursor.lua
+++ Windows/mpv/scripts/hidecursor.lua
@@ -1,11 +1,11 @@
 function handle_move(prop, m)
-	if mp.get_property_bool("fullscreen") then
-		local w, h = mp.get_osd_size()
-		if m.y < (h * 0.30) then
-			mp.set_property("cursor-autohide", "always")
-		else
-			mp.set_property("cursor-autohide", 1000)
-		end
-	end
+    if mp.get_property_bool("fullscreen") then
+        local w, h = mp.get_osd_size()
+        if m.y < (h * 0.30) then
+            mp.set_property("cursor-autohide", "always")
+        else
+            mp.set_property("cursor-autohide", 1000)
+        end
+    end
 end
 mp.observe_property("mouse-pos", "native", handle_move)
