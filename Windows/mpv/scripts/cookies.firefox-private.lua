d771bc79316308961739e72c14787f50470f6205
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-06-25 15:41:48 +0300

    cookies.firefox-private.lua: reformat

--- Windows/mpv/scripts/cookies.firefox-private.lua
+++ Windows/mpv/scripts/cookies.firefox-private.lua
@@ -1,1 +1,2 @@
-mp.commandv('no-osd', 'change-list', 'ytdl-raw-options', 'append', 'cookies=' .. mp.command_native({'expand-path', '~~/cookies.firefox-private.txt'}))
+mp.commandv('no-osd', 'change-list', 'ytdl-raw-options', 'append',
+    'cookies=' .. mp.command_native({'expand-path', '~~/cookies.firefox-private.txt'}))
