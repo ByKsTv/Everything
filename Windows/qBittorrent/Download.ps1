5b2f7d1f0b17b79f32a5a1b401e6be082c8cf5db
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-07-01 21:24:31 +0300

    qbit create folder if not exist

--- Windows/qBittorrent/Download.ps1
+++ Windows/qBittorrent/Download.ps1
@@ -6,7 +6,9 @@ $qBittorrent.DownloadFile('https://sourceforge.net/projects/qbittorrent/files/la
 Write-Host 'qBittorrent: Installing' -ForegroundColor green -BackgroundColor black
 Start-Process $ENV:temp\qBittorrent.exe -ArgumentList '/S'
 
+Write-Host 'qBittorrent: Using custom settings' -ForegroundColor green -BackgroundColor black
 if (!(Test-Path -Path "$env:APPDATA\qBittorrent\qBittorrent.ini")) {
-    Write-Host 'qBittorrent: Using custom settings' -ForegroundColor green -BackgroundColor black
+    Write-Host 'qBittorrent: Creating qBittorrent folder' -ForegroundColor green -BackgroundColor black
+    New-Item -Path "$env:APPDATA\qBittorrent" -Value qBittorrent -ItemType Directory
     (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/qBittorrent/qBittorrent.ini', "$env:APPDATA\qBittorrent\qBittorrent.ini")
 }
