4117046ea29bcd4d60b419781ebfe91debfd33be
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-07-05 05:36:21 +0300

    anydesk add custom settings

--- Windows/AnyDesk/Download.ps1
+++ Windows/AnyDesk/Download.ps1
@@ -3,3 +3,9 @@ Write-Host 'AnyDesk: Downloading' -ForegroundColor green -BackgroundColor black
 
 Write-Host 'AnyDesk: Installing' -ForegroundColor green -BackgroundColor black
 Start-Process -FilePath $env:TEMP\AnyDesk.exe -ArgumentList '--install "C:\Program Files (x86)\AnyDesk" --create-shortcuts --create-desktop-icon --silent'
+
+Write-Host 'AnyDesk: Optional Offer - Recommended by AnyDesk: Decline' -ForegroundColor green -BackgroundColor black
+if ((Test-Path -Path 'HKCU:\SOFTWARE\Google\No Chrome Offer Until') -ne $true) {
+ New-Item 'HKCU:\SOFTWARE\Google\No Chrome Offer Until' -Force 
+}
+New-ItemProperty -Path 'HKCU:\SOFTWARE\Google\No Chrome Offer Until' -Name 'AnyDesk Software GmbH' -Value 30241008 -PropertyType DWord -Force
