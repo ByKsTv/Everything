125991cd7869419f0b7b7c74d409a471ec3351bc
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-07-08 03:01:46 +0300

    subtitledit: dont create schedule task autoupdate untill mismateched version fixed

--- Windows/Subtitle_Edit/Download.ps1
+++ Windows/Subtitle_Edit/Download.ps1
@@ -1,22 +1,21 @@
-$SubtitleEdit = 'Subtitle Edit Updater'
-$SubtitleEdit_Exists = Get-ScheduledTask | Where-Object { $_.TaskName -like $SubtitleEdit }
-if (!($SubtitleEdit_Exists)) {
-    Write-Host "Subtitle Edit: Task Scheduler: Adding $SubtitleEdit" -ForegroundColor green -BackgroundColor black
-    $SubtitleEdit_Principal = New-ScheduledTaskPrincipal -UserId "$env:computername\$env:USERNAME" -RunLevel Highest
-    $SubtitleEdit_Action = New-ScheduledTaskAction -Execute powershell.exe -Argument "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Subtitle_Edit/Download.ps1')"
-    $SubtitleEdit_Trigger = New-ScheduledTaskTrigger -AtLogOn
-    $SubtitleEdit_Settings = New-ScheduledTaskSettingsSet -Compatibility Win8 -StartWhenAvailable
-    $SubtitleEdit_Parameters = @{
-        TaskName  = $SubtitleEdit
-        Principal = $SubtitleEdit_Principal
-        Action    = $SubtitleEdit_Action
-        Trigger   = $SubtitleEdit_Trigger
-        Settings  = $SubtitleEdit_Settings
-    }
-    Register-ScheduledTask @SubtitleEdit_Parameters -Force
-}
+# $SubtitleEdit = 'Subtitle Edit Updater'
+# $SubtitleEdit_Exists = Get-ScheduledTask | Where-Object { $_.TaskName -like $SubtitleEdit }
+# if (!($SubtitleEdit_Exists)) {
+#     Write-Host "Subtitle Edit: Task Scheduler: Adding $SubtitleEdit" -ForegroundColor green -BackgroundColor black
+#     $SubtitleEdit_Principal = New-ScheduledTaskPrincipal -UserId "$env:computername\$env:USERNAME" -RunLevel Highest
+#     $SubtitleEdit_Action = New-ScheduledTaskAction -Execute powershell.exe -Argument "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Subtitle_Edit/Download.ps1')"
+#     $SubtitleEdit_Trigger = New-ScheduledTaskTrigger -AtLogOn
+#     $SubtitleEdit_Settings = New-ScheduledTaskSettingsSet -Compatibility Win8 -StartWhenAvailable
+#     $SubtitleEdit_Parameters = @{
+#         TaskName  = $SubtitleEdit
+#         Principal = $SubtitleEdit_Principal
+#         Action    = $SubtitleEdit_Action
+#         Trigger   = $SubtitleEdit_Trigger
+#         Settings  = $SubtitleEdit_Settings
+#     }
+#     Register-ScheduledTask @SubtitleEdit_Parameters -Force
+# }
 
-# Not ready since there's a mismatch version numbers
 $SubtitleEditInstalledVer = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\SubtitleEdit_is1' -ErrorAction SilentlyContinue).DisplayVersion
 $SubtitleEditLatestVer = (Invoke-RestMethod -Method GET -Uri 'https://api.github.com/repos/SubtitleEdit/subtitleedit/releases/latest').tag_name
 
