b9646b7072ed9ea300d6d219d8f872296acf2a60
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-07-15 16:09:45 +0300

    mpv auto delete file fix

--- Windows/mpv/scripts/delete_file_auto.lua
+++ Windows/mpv/scripts/delete_file_auto.lua
@@ -1,11 +1,7 @@
 local function read_time(_, time_pos)
-    local url = mp.get_property("path")
-    if string.match(url, "http") then
-    else
-        if time_pos and time_pos > mp.get_property_native('duration', math.huge) - 15 then
-            mp.command('script-binding delete_file/delete_file')
-            mp.unobserve_property(read_time)
-        end
+    if time_pos and time_pos > mp.get_property_native('duration', math.huge) - 15 then
+        mp.command('script-binding delete_file/delete_file')
+        mp.unobserve_property(read_time)
     end
 end
 
