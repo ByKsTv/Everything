a9d70114efb0bd2baada22e136264b3cd7f6ccd2
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-07-19 04:57:34 +0300

    chrome add group policy

--- Windows/Chrome/Download.ps1
+++ Windows/Chrome/Download.ps1
@@ -4,11 +4,23 @@ Write-Host 'Google Chrome: Downloading' -ForegroundColor green -BackgroundColor 
 Write-Host 'Google Chrome: Installing' -ForegroundColor green -BackgroundColor black
 Start-Process $env:TEMP\googlechromestandaloneenterprise64.msi -ArgumentList '/quiet' -Wait
 
+Write-Host 'Google Chrome: Downloading group policy' -ForegroundColor green -BackgroundColor black
+(New-Object System.Net.WebClient).DownloadFile('https://dl.google.com/dl/edgedl/chrome/policy/policy_templates.zip', "$env:TEMP\policy_templates_chrome.zip")
+
+Write-Host 'Google Chrome: Extracting group policy' -ForegroundColor green -BackgroundColor black
+Expand-Archive -Path "$env:TEMP\policy_templates_chrome.zip" -DestinationPath "$env:TEMP\policy_templates_chrome"
+
+Write-Host 'Google Chrome: Importing group policy' -ForegroundColor green -BackgroundColor black
+Move-Item -Path "$env:TEMP\policy_templates_chrome\windows\admx\chrome.admx" -Destination "$env:windir\PolicyDefinitions"
+Move-Item -Path "$env:TEMP\policy_templates_chrome\windows\admx\google.admx" -Destination "$env:windir\PolicyDefinitions"
+Move-Item -Path "$env:TEMP\policy_templates_chrome\windows\admx\en-US\chrome.adml" -Destination "$env:windir\PolicyDefinitions\en-US"
+Move-Item -Path "$env:TEMP\policy_templates_chrome\windows\admx\en-US\google.adml" -Destination "$env:windir\PolicyDefinitions\en-US"
+
 Write-Host 'Google Chrome: Starting' -ForegroundColor green -BackgroundColor black
 Start-Process "$env:ProgramFiles\Google\Chrome\Application\chrome.exe"
 
 Write-Host 'Google Chrome: Opening default apps on Windows Settings' -ForegroundColor green -BackgroundColor black
-Start-Process "ms-settings:defaultapps"
+Start-Process 'ms-settings:defaultapps'
 
 Write-Host 'Google Chrome: Waiting for user to set up default browser' -ForegroundColor green -BackgroundColor black
 Add-Type -AssemblyName System.Windows.Forms
