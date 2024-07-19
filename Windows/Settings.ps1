69e72cfc1bffcb5ce63e01acc22b18d6495968ad
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-07-19 20:07:08 +0300

    install directx

--- Windows/Settings.ps1
+++ Windows/Settings.ps1
@@ -2217,6 +2217,12 @@ New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name 
 Start-Process -FilePath "$env:TEMP\VC_redist.x86.exe" -ArgumentList '/install /quiet /norestart'
 (New-Object System.Net.WebClient).DownloadFile('https://aka.ms/vs/17/release/VC_redist.x64.exe', "$env:TEMP\VC_redist.x64.exe")
 Start-Process -FilePath "$env:TEMP\VC_redist.x64.exe" -ArgumentList '/install /quiet /norestart'
+
+Write-Host 'DirectX: Downloading' -ForegroundColor green -BackgroundColor black
+(New-Object System.Net.WebClient).DownloadFile((Invoke-WebRequest -UseBasicParsing -Uri 'https://www.microsoft.com/en-us/download/details.aspx?id=35' | Select-Object -ExpandProperty Links | Where-Object { ($_.outerHTML -match 'dxwebsetup.exe') } | Select-Object -First 1 | Select-Object -ExpandProperty href), "$env:TEMP\dxwebsetup.exe")
+
+Write-Host 'DirectX: Installing' -ForegroundColor green -BackgroundColor black
+Start-Process -FilePath $env:TEMP\dxwebsetup.exe -ArgumentList '/Q' -Wait
 # RKNBypass -Disable
 # Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name AutoConfigURL -Force # Error
 # PreventEdgeShortcutCreation -Channels Stable, Beta, Dev, Canary
