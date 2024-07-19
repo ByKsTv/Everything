f2eb3e67c1e1a1350fc9820ae12e2dcc51267d44
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-07-19 03:47:01 +0300

    chrome add default browser

--- Windows/Chrome/Download.ps1
+++ Windows/Chrome/Download.ps1
@@ -6,3 +6,12 @@ Start-Process $env:TEMP\googlechromestandaloneenterprise64.msi -ArgumentList '/q
 
 Write-Host 'Google Chrome: Starting' -ForegroundColor green -BackgroundColor black
 Start-Process "$env:ProgramFiles\Google\Chrome\Application\chrome.exe"
+
+Write-Host 'Google Chrome: Opening default apps on Windows Settings' -ForegroundColor green -BackgroundColor black
+Start-Process "ms-settings:defaultapps"
+
+Write-Host 'Google Chrome: Waiting for user to set up default browser' -ForegroundColor green -BackgroundColor black
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show('Set up Google Chrome as Default Browser.
+
+Press OK after Finished.' , 'Default Browser Notification' , 0, 64)
