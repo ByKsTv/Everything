89f352e94ec84238051a7af167a8254c6c1dfd41
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-07-19 04:57:42 +0300

    firefox add group policy

--- Windows/Firefox/Download.ps1
+++ Windows/Firefox/Download.ps1
@@ -18,6 +18,18 @@ Write-Host 'Mozilla Firefox: Downloading' -ForegroundColor green -BackgroundColo
 Write-Host 'Mozilla Firefox: Installing' -ForegroundColor green -BackgroundColor black
 Start-Process $env:TEMP\firefox.exe -ArgumentList '/S' -Wait
 
+Write-Host 'Mozilla Firefox: Downloading group policy' -ForegroundColor green -BackgroundColor black
+(New-Object System.Net.WebClient).DownloadFile(((Invoke-RestMethod -Method GET -Uri 'https://api.github.com/repos/mozilla/policy-templates/releases/latest').assets | Where-Object name -Like 'policy_templates*' ).browser_download_url, "$env:TEMP\policy_templates_firefox.zip")
+
+Write-Host 'Mozilla Firefox: Extracting group policy' -ForegroundColor green -BackgroundColor black
+Expand-Archive -Path "$env:TEMP\policy_templates_firefox.zip" -DestinationPath "$env:TEMP\policy_templates_firefox"
+
+Write-Host 'Mozilla Firefox: Importing group policy' -ForegroundColor green -BackgroundColor black
+Move-Item -Path "$env:TEMP\policy_templates_firefox\windows\firefox.admx" -Destination "$env:windir\PolicyDefinitions"
+Move-Item -Path "$env:TEMP\policy_templates_firefox\windows\mozilla.admx" -Destination "$env:windir\PolicyDefinitions"
+Move-Item -Path "$env:TEMP\policy_templates_firefox\windows\en-US\firefox.adml" -Destination "$env:windir\PolicyDefinitions\en-US"
+Move-Item -Path "$env:TEMP\policy_templates_firefox\windows\en-US\mozilla.adml" -Destination "$env:windir\PolicyDefinitions\en-US"
+
 Write-Host 'Mozilla Firefox: Deleting Desktop Shortcut' -ForegroundColor green -BackgroundColor black
 if ((Test-Path -Path "$env:PUBLIC\Desktop\Firefox.lnk") -eq $true) {
     Remove-Item -Path ("$env:PUBLIC\Desktop\Firefox.lnk")
