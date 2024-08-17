7c882aa3ed527ccaad1ac3b523d79d40032bd140
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-08-17 10:08:29 +0300

    step3: directx update install

--- Windows/Step3.ps1
+++ Windows/Step3.ps1
@@ -2,9 +2,11 @@ Write-Host 'Step3: Task Scheduler: Removing current step' -ForegroundColor green
 Unregister-ScheduledTask -TaskName Step3 -Confirm:$false
 
 Write-Host 'Step3: DirectX: Downloading' -ForegroundColor green -BackgroundColor black
-(New-Object System.Net.WebClient).DownloadFile((Invoke-WebRequest -UseBasicParsing -Uri 'https://www.microsoft.com/en-us/download/details.aspx?id=35' | Select-Object -ExpandProperty Links | Where-Object { ($_.outerHTML -match 'dxwebsetup.exe') } | Select-Object -First 1 | Select-Object -ExpandProperty href), "$env:TEMP\dxwebsetup.exe")
+(New-Object System.Net.WebClient).DownloadFile((Invoke-WebRequest -UseBasicParsing -Uri 'https://www.microsoft.com/en-us/download/details.aspx?id=8109' | Select-Object -ExpandProperty Links | Where-Object { ($_.outerHTML -match 'directx_Jun2010_redist.exe') } | Select-Object -First 1 | Select-Object -ExpandProperty href), "$env:TEMP\directx_Jun2010_redist.exe")
+Write-Host 'Step3: DirectX: Extracting' -ForegroundColor green -BackgroundColor black
+Start-Process -FilePath $env:TEMP\directx_Jun2010_redist.exe -ArgumentList "/Q /T:$env:TEMP\directx_Jun2010_redist" -Wait
 Write-Host 'Step3: DirectX: Installing' -ForegroundColor green -BackgroundColor black
-Start-Process -FilePath $env:TEMP\dxwebsetup.exe -ArgumentList '/Q'
+Start-Process -FilePath "$env:TEMP\directx_Jun2010_redist\DXSETUP.exe" -ArgumentList "/silent"
 
 Write-Host 'Step3: .NET: Initiating' -ForegroundColor green -BackgroundColor black
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/.NET/Download.ps1')
