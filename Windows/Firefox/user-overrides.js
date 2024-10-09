4fcd61f4f0fe02308c7c1b2949cb80e3de0e1eb8
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-10-09 19:43:30 +0300

    firefox: remove tab image preview and firefox labs

--- Windows/Firefox/user-overrides.js
+++ Windows/Firefox/user-overrides.js
@@ -83,5 +83,11 @@ user_pref("browser.preferences.moreFromMozilla", false);
 // Disable Firefox is full screen
 user_pref("full-screen-api.warning.timeout", 0);
 
+// Disable tab image preview
+user_pref("browser.tabs.hoverPreview.enabled", false);
+
+// Disable Firefox Labs
+user_pref("browser.preferences.experimental", false);
+
 // Custom UI
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"save-to-pocket-button\",\"downloads-button\",\"unified-extensions-button\",\"fxa-toolbar-menu-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\"],\"currentVersion\":20,\"newElementCount\":5}");
