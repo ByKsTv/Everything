52cd39f66f1740acfcb3a1a3d35280065588cdb9
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-10-19 00:09:11 +0300

    qbit download script redesign

--- Windows/qBittorrent/Download.ps1
+++ Windows/qBittorrent/Download.ps1
@@ -1,40 +1,33 @@
-$qBittorrent = 'qBittorrent Updater'
-$qBittorrent_Exists = Get-ScheduledTask | Where-Object { $_.TaskName -like $qBittorrent }
-if (!($qBittorrent_Exists)) {
-    Write-Host "qBittorrent: Task Scheduler: Adding $qBittorrent" -ForegroundColor green -BackgroundColor black
-    $qBittorrent_Principal = New-ScheduledTaskPrincipal -UserId "$env:computername\$env:USERNAME" -RunLevel Highest
-    $qBittorrent_Action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument "/C start /MIN powershell -WindowStyle Minimized Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/qBittorrent/Download.ps1')"
-    $qBittorrent_Trigger = New-ScheduledTaskTrigger -AtLogOn
-    $qBittorrent_Settings = New-ScheduledTaskSettingsSet -Compatibility Win8 -StartWhenAvailable
-    $qBittorrent_Parameters = @{
-        TaskName  = $qBittorrent
-        Principal = $qBittorrent_Principal
-        Action    = $qBittorrent_Action
-        Trigger   = $qBittorrent_Trigger
-        Settings  = $qBittorrent_Settings
-    }
-    Register-ScheduledTask @qBittorrent_Parameters -Force
+$qBittorrent_TaskName = 'qBittorrent Updater'
+if (-not (Get-ScheduledTask -TaskName $qBittorrent_TaskName -ErrorAction SilentlyContinue)) {
+    [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Task Scheduler: Adding '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$qBittorrent_TaskName'"); [Console]::ResetColor(); [Console]::WriteLine()
+    $qBittorrent_TaskAction = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument "/C start /MIN powershell -WindowStyle Minimized Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/qBittorrent/Download.ps1')"
+    $qBittorrent_TaskTrigger = New-ScheduledTaskTrigger -AtLogOn
+    $qBittorrent_TaskPrincipal = New-ScheduledTaskPrincipal -UserId "$env:computername\$env:USERNAME" -RunLevel Highest
+    $qBittorrent_TaskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
+    Register-ScheduledTask -TaskName $qBittorrent_TaskName -Action $qBittorrent_TaskAction -Trigger $qBittorrent_TaskTrigger -Principal $qBittorrent_TaskPrincipal -Settings $qBittorrent_TaskSettings -Force
 }
 
-Write-Host 'qBittorrent: Getting current version' -ForegroundColor green -BackgroundColor black
-$qBittorrent_Installed = Get-Package -Name 'qBittorrent' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty 'Version'
+$qBittorrent_InstalledVersion = (Get-Package -Name 'qBittorrent' -ErrorAction SilentlyContinue).Version
+$qBittorrent_LatestVersion = ((Invoke-RestMethod https://api.github.com/repos/qbittorrent/qbittorrent/tags).Name | Where-Object { $_ -NotMatch 'beta' -And $_ -NotMatch 'rc' } | Select-Object -First 1).Replace('release-', '')
 
-Write-Host 'qBittorrent: Getting latest version' -ForegroundColor green -BackgroundColor black
-$qBittorrent_Latest = ((Invoke-RestMethod https://api.github.com/repos/qbittorrent/qbittorrent/tags).Name | Where-Object { $_ -NotMatch 'beta' -And $_ -NotMatch 'rc' } | Select-Object -First 1).Replace('release-', '')
-
-if (($null -eq $qBittorrent_Installed) -or ($qBittorrent_Installed -notmatch $qBittorrent_Latest)) {
-    Write-Host "qBittorrent: Downloading $qBittorrent_Latest" -ForegroundColor green -BackgroundColor black
-    $qBittorrent_SourceForge = (Invoke-WebRequest -UseBasicParsing -Uri 'https://www.qbittorrent.org/download' | Select-Object -ExpandProperty Links | Where-Object { ($_.outerHTML -match 'SourceForge') } | Select-Object -First 1 | Select-Object -ExpandProperty href)
-    $qBittorrentURL = (Invoke-WebRequest -Uri "$qBittorrent_SourceForge" -UseBasicParsing).links.'data-release-url'
-    (New-Object System.Net.WebClient).DownloadFile("$qBittorrentURL", "$ENV:temp\qBittorrent.exe")
-    
-    Write-Host "qBittorrent: Installing $qBittorrent_Latest" -ForegroundColor green -BackgroundColor black
-    Start-Process "$ENV:temp\qBittorrent.exe" -ArgumentList '/S' -Wait
-    
-    if (!(Test-Path -Path "$env:APPDATA\qBittorrent\qBittorrent.ini")) {
-        Write-Host 'qBittorrent: Creating qBittorrent folder' -ForegroundColor green -BackgroundColor black
-        New-Item -Path "$env:APPDATA\qBittorrent" -ItemType Directory -Force
-        Write-Host 'qBittorrent: Using custom settings' -ForegroundColor green -BackgroundColor black
-        (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/qBittorrent/qBittorrent.ini', "$env:APPDATA\qBittorrent\qBittorrent.ini")
+if (($null -eq $qBittorrent_InstalledVersion) -or ($qBittorrent_InstalledVersion -notmatch $qBittorrent_LatestVersion)) {
+    $qBittorrent_RemoteINI = 'https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/qBittorrent/qBittorrent.ini'
+    $qBittorrent_LocalINI = "$env:APPDATA\qBittorrent\qBittorrent.ini"
+    if (-not (Test-Path -Path $qBittorrent_LocalINI)) {
+        New-Item -Path $qBittorrent_LocalINI -ItemType File -Force
+        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write("Downloading 'qBittorrent' custom settings from "); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$qBittorrent_RemoteINI'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$qBittorrent_LocalINI'"); [Console]::ResetColor(); [Console]::WriteLine()
+        (New-Object System.Net.WebClient).DownloadFile($qBittorrent_RemoteINI, $qBittorrent_LocalINI)
     }
+    
+    $qBittorrent_SourceForge = ((Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/qbittorrent/qBittorrent-website/refs/heads/master/_site/download.html').Links | Where-Object { ($_.outerHTML -match 'sourceforge' -and $_.outerHTML -match '.exe' -and $_.outerHTML -match 'lt20' ) } | Select-Object -First 1).href
+    $qBittorrent_DDL = ((Invoke-WebRequest -Uri $qBittorrent_SourceForge -UseBasicParsing).links | Where-Object { $_.'data-release-url' -ne $null }).'data-release-url'
+    $qBittorrent_Filename = [System.IO.Path]::GetFileName(([System.Uri]$qBittorrent_DDL).AbsolutePath)
+    $qBittorrent_SavePath = [System.IO.Path]::Combine($env:TEMP, $qBittorrent_Filename)
+    [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write("Downloading 'qBittorrent' from "); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$qBittorrent_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$qBittorrent_SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
+    (New-Object System.Net.WebClient).DownloadFile($qBittorrent_DDL, $qBittorrent_SavePath)
+    
+    $qBittorrent_Argument = '/S'
+    [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write("Installing 'qBittorrent' from "); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$qBittorrent_SavePath'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' with '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$qBittorrent_Argument'"); [Console]::ResetColor(); [Console]::WriteLine()
+    Start-Process $qBittorrent_SavePath -ArgumentList $qBittorrent_Argument
 }
