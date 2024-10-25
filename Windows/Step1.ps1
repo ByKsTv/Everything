fde5aa7ee53f3f38712401fe3c9d74d76b0984ac
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-10-25 21:25:00 +0300

    small fix

--- Windows/Step1.ps1
+++ Windows/Step1.ps1
@@ -1,8 +1,7 @@
 $NextStep_TaskName = 'Step2'
-$NextStep_SavePath = [System.IO.Path]::Combine($env:TEMP, $NextStep_TaskName.ps1)
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/$NextStep_TaskName.ps1", "$NextStep_SavePath")
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/$NextStep_TaskName.ps1", "$env:TEMP\$NextStep_TaskName.ps1")
 [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Task Scheduler: Adding '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$NextStep_TaskName'"); [Console]::ResetColor(); [Console]::WriteLine()
-$NextStep_TaskAction = New-ScheduledTaskAction -Execute powershell.exe -Argument "-WindowStyle Maximized -ExecutionPolicy Bypass -File $NextStep_SavePath"
+$NextStep_TaskAction = New-ScheduledTaskAction -Execute powershell.exe -Argument "-WindowStyle Maximized -ExecutionPolicy Bypass -File $env:TEMP\$NextStep_TaskName.ps1"
 $NextStep_TaskTrigger = New-ScheduledTaskTrigger -AtLogOn
 $NextStep_TaskPrincipal = New-ScheduledTaskPrincipal -UserId "$env:computername\$env:USERNAME" -RunLevel Highest
 $NextStep_TaskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
