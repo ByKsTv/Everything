eaec40c4377b78a523108bce2cd8cb492defae63
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-11-03 08:53:18 +0200

    a

--- Windows/Discord/Download.ps1
+++ Windows/Discord/Download.ps1
@@ -23,7 +23,7 @@ if ($null -eq $Discord_InstalledVersion -or $Discord_InstalledVersion -notmatch 
 
     $Discord_InstallerPopup = 'Discord Updater'
     [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Waiting for window '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$Discord_InstallerPopup'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to open '); [Console]::ResetColor(); [Console]::WriteLine()
-    while (!(Get-Process | Where-Object { $_.MainWindowTitle -eq $Discord_InstallerPopup } )) {
+    while (-not (Get-Process | Where-Object { $_.MainWindowTitle -eq $Discord_InstallerPopup } )) {
         Start-Sleep -Milliseconds 1000
     }
     [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Waiting for window '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$Discord_InstallerPopup'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to close '); [Console]::ResetColor(); [Console]::WriteLine()
