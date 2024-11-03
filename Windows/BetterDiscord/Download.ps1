93dea3cf54c26a0e30d5ef31f338fcc386b5a00d
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-11-03 06:45:32 +0200

    a

--- Windows/BetterDiscord/Download.ps1
+++ Windows/BetterDiscord/Download.ps1
@@ -9,6 +9,9 @@ if (-not (Get-ScheduledTask -TaskName $BetterDiscord_TaskName -ErrorAction Silen
 }
 
 $Discord_TaskName = 'Discord Client Updater'
+if (!(Get-ScheduledTask -TaskName $Discord_TaskName -ErrorAction SilentlyContinue)) {
+    Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Discord/Download.ps1')
+}
 if (Get-ScheduledTask -TaskName $Discord_TaskName -ErrorAction SilentlyContinue) {
     Start-Sleep -Milliseconds 1000
     while ((Get-ScheduledTask -TaskName $Discord_TaskName).State -eq 'Running') {
