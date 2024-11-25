70261ce8b6c7b8a13d14c4461906a73f2d36ab32
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-11-25 20:01:29 +0200

    redesign policy template for acrobat

--- Windows/Software_Selection.ps1
+++ Windows/Software_Selection.ps1
@@ -1294,16 +1294,20 @@ $SoftwareSelection_Form_OK.Add_Click{
     if ($SoftwareSelection_CheckBoxes['Adobe Acrobat Pro'].Checked) {
         # https://www.adobe.com/devnet-docs/acrobatetk/tools/PrefRef/Windows/index.html
         # https://www.adobe.com/devnet-docs/acrobatetk/tools/PrefRef/Windows/FeatureLockDown.html#idkeyname_1_13262
-        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Adobe Acrobat Pro: Downloading group policy'); [Console]::ResetColor(); [Console]::WriteLine()
-        (New-Object System.Net.WebClient).DownloadFile('https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/AcrobatADMTemplate.zip', "$env:TEMP\policy_templates_acrobat.zip")
+        $AdobeAcrobat_PolicyTemplates_DDL = 'https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/misc/AcrobatADMTemplate.zip'
+        $AdobeAcrobat_PolicyTemplates_Filename = [IO.Path]::GetFileName(([URI]$AdobeAcrobat_PolicyTemplates_DDL).AbsolutePath)
+        $AdobeAcrobat_PolicyTemplates_SavePath = [IO.Path]::Combine($env:TEMP, $AdobeAcrobat_PolicyTemplates_Filename)
+        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AdobeAcrobat_PolicyTemplates_Filename'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AdobeAcrobat_PolicyTemplates_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AdobeAcrobat_PolicyTemplates_SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
+        (New-Object System.Net.WebClient).DownloadFile($AdobeAcrobat_PolicyTemplates_DDL, $AdobeAcrobat_PolicyTemplates_SavePath)
         
-        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Adobe Acrobat Pro: Extracting group policy'); [Console]::ResetColor(); [Console]::WriteLine()
-        Expand-Archive -Path "$env:TEMP\policy_templates_acrobat.zip" -DestinationPath "$env:TEMP\policy_templates_acrobat" -ErrorAction SilentlyContinue
+        $AdobeAcrobat_PolicyTemplates_Dir = $AdobeAcrobat_PolicyTemplates_SavePath.TrimEnd('.zip')
+        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Extracting '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AdobeAcrobat_PolicyTemplates_Filename'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AdobeAcrobat_PolicyTemplates_SavePath'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AdobeAcrobat_PolicyTemplates_Dir'"); [Console]::ResetColor(); [Console]::WriteLine()
+        Expand-Archive -Path $AdobeAcrobat_PolicyTemplates_SavePath -DestinationPath $AdobeAcrobat_PolicyTemplates_Dir -Force
         
-        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Adobe Acrobat Pro: Importing group policy'); [Console]::ResetColor(); [Console]::WriteLine()
-        Move-Item -Path "$env:TEMP\policy_templates_acrobat\AcrobatDC.adm" -Destination "$env:windir\PolicyDefinitions" -ErrorAction SilentlyContinue
-        Move-Item -Path "$env:TEMP\policy_templates_acrobat\AcrobatDC.admx" -Destination "$env:windir\PolicyDefinitions" -ErrorAction SilentlyContinue
-        Move-Item -Path "$env:TEMP\policy_templates_acrobat\en-US\AcrobatDC.adml" -Destination "$env:windir\PolicyDefinitions\en-US" -ErrorAction SilentlyContinue
+        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Installing '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Adobe Acrobat Policy Templates'"); [Console]::ResetColor(); [Console]::WriteLine()
+        Copy-Item "$AdobeAcrobat_PolicyTemplates_Dir\*.admx" "$env:windir\PolicyDefinitions" -Force
+        Copy-Item "$AdobeAcrobat_PolicyTemplates_Dir\*.adm" "$env:windir\PolicyDefinitions" -Force
+        Copy-Item "$AdobeAcrobat_PolicyTemplates_Dir\en-US\*.adml" "$env:windir\PolicyDefinitions\en-US" -Force
         
         [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Adobe Acrobat Pro: User Configuration: Administrative Templates: Adobe Acrobat DC: Preferences: General: Display splash screen at launch: Disabled'); [Console]::ResetColor(); [Console]::WriteLine()
         if ((Test-Path -LiteralPath 'HKCU:\SOFTWARE\Adobe\Adobe Acrobat\DC\Originals') -ne $true) {
