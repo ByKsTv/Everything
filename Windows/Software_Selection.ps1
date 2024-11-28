06f8119cd158979a215f814063f7af71483a5f3f
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-11-28 05:25:16 +0200

    anydesk improve download

--- Windows/Software_Selection.ps1
+++ Windows/Software_Selection.ps1
@@ -841,20 +841,24 @@ $SoftwareSelection_Form_OK.Add_Click{
 ad.discovery.show_tile=0
 ad.ui.show_tile.telemetry=false
 '@
-$AnyDesk_LocalCFG = [IO.Path]::Combine($env:APPDATA, 'AnyDesk', 'user.conf')
-New-Item -Path $AnyDesk_LocalCFG -ItemType File -Value $AnyDesk_Settings -Force
+        $AnyDesk_LocalCFG = [IO.Path]::Combine($env:APPDATA, 'AnyDesk', 'user.conf')
+        New-Item -Path $AnyDesk_LocalCFG -ItemType File -Value $AnyDesk_Settings -Force
 
-        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Software Selection: AnyDesk: Downloading'); [Console]::ResetColor(); [Console]::WriteLine()
-        (New-Object System.Net.WebClient).DownloadFile('https://download.anydesk.com/AnyDesk.exe', "$env:TEMP\AnyDesk.exe")
-        
-        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Software Selection: AnyDesk: Installing'); [Console]::ResetColor(); [Console]::WriteLine()
-        Start-Process -FilePath $env:TEMP\AnyDesk.exe -ArgumentList '--install "C:\Program Files (x86)\AnyDesk" --create-shortcuts --create-desktop-icon --silent'
-        
         [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Software Selection: AnyDesk: Optional Offer - Recommended by AnyDesk: Decline'); [Console]::ResetColor(); [Console]::WriteLine()
         if ((Test-Path -Path 'HKCU:\SOFTWARE\Google\No Chrome Offer Until') -ne $true) {
             New-Item 'HKCU:\SOFTWARE\Google\No Chrome Offer Until' -Force 
         }
         New-ItemProperty -Path 'HKCU:\SOFTWARE\Google\No Chrome Offer Until' -Name 'AnyDesk Software GmbH' -Value 30241008 -PropertyType DWord -Force
+
+        $AnyDesk_DDL = 'https://download.anydesk.com/AnyDesk.exe'
+        $AnyDesk_Filename = [IO.Path]::GetFileName(([URI]$AnyDesk_DDL).AbsolutePath)
+        $AnyDesk_SavePath = [IO.Path]::Combine($env:TEMP, $AnyDesk_Filename)
+        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'AnyDesk'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AnyDesk_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AnyDesk_SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
+        (New-Object System.Net.WebClient).DownloadFile($AnyDesk_DDL, $AnyDesk_SavePath)
+
+        $AnyDesk_Argument = '--install "C:\Program Files (x86)\AnyDesk" --create-shortcuts --create-desktop-icon --silent'
+        [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Installing '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'AnyDesk'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AnyDesk_SavePath'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' with '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$AnyDesk_Argument'"); [Console]::ResetColor(); [Console]::WriteLine()
+        Start-Process $AnyDesk_SavePath -ArgumentList $AnyDesk_Argument
     }
 
     if ($SoftwareSelection_CheckBoxes['Valorant'].Checked) {
