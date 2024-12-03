e52da076dc265b2c9faaf5d57b70024f9ba461e0
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-04 00:45:51 +0200

    software selection active detect win server or client

--- Windows/Software_Selection.ps1
+++ Windows/Software_Selection.ps1
@@ -611,7 +611,12 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/7Zip/Download.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['Activate Windows'].Checked) {
-        & ([ScriptBlock]::Create(((New-Object System.Net.WebClient).DownloadString('https://get.activated.win/')))) /HWID
+        if ((Get-WmiObject -Class Win32_OperatingSystem).ProductType -eq 3) {
+            & ([ScriptBlock]::Create(((New-Object System.Net.WebClient).DownloadString('https://get.activated.win/')))) /KMS38
+        }
+        else {
+            & ([ScriptBlock]::Create(((New-Object System.Net.WebClient).DownloadString('https://get.activated.win/')))) /HWID
+        }       
     }
     if ($SoftwareSelection_CheckBoxes['ADB'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_ADB.ps1') 
