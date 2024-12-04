1c7b4afa3b0eab0add5a322e1c704210c75bfede
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-04 08:43:51 +0200

    fix

--- Windows/Server/Step1.ps1
+++ Windows/Server/Step1.ps1
@@ -516,10 +516,7 @@ if (-not (Test-Path -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Windo
 New-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate' -Name 'AutoDownload' -Value 2 -PropertyType DWord -Force
 
 # O&O ShutUp10++: Local Machine: Windows Update: Disable Windows Updates for other products (e.g. Microsoft Office): Off
-$DisableWindowsUpdatesforotherproducts = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\7971f918-a847-4430-9279-4a52d1efe18d'
-if (Test-Path $DisableWindowsUpdatesforotherproducts -and (Get-ItemProperty $DisableWindowsUpdatesforotherproducts).RegisteredWithAU -ne $null) {
-    Remove-ItemProperty -Path $DisableWindowsUpdatesforotherproducts -Name 'RegisteredWithAU' -Force
-}
+# Remove-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\7971f918-a847-4430-9279-4a52d1efe18d' -Name 'RegisteredWithAU' -Force
 
 # O&O ShutUp10++: Local Machine: Privacy: Disable Windows Error Reporting: On
 New-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting' -Name 'Disabled' -Value 1 -PropertyType DWord -Force
