882cb1eb51e4b55aad630ada3d810ff5053d183d
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-04 22:50:58 +0200

    move settings down to apply them for some reason

--- Windows/Scripts/Install_Battle.net.ps1
+++ Windows/Scripts/Install_Battle.net.ps1
@@ -1,3 +1,17 @@
+$BattleNET_DDL = 'https://downloader.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe'
+$BattleNET_Filename = 'Battle.net-Setup.exe'
+$BattleNET_SavePath = [IO.Path]::Combine($env:TEMP, $BattleNET_Filename)
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
+(New-Object System.Net.WebClient).DownloadFile($BattleNET_DDL, $BattleNET_SavePath)
+                
+$BattleNET_Argument = '--lang=enUS --installpath="C:\Program Files (x86)\Battle.net"'
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Installing '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_SavePath'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' with '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_Argument'"); [Console]::ResetColor(); [Console]::WriteLine()
+Start-Process $BattleNET_SavePath -ArgumentList $BattleNET_Argument -Wait
+
+if ($null -ne (Get-Item -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run').GetValue('Battle.net')) {
+	Remove-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'Battle.net'
+}
+
 $BattleNet_Config = [IO.Path]::Combine($env:APPDATA, 'Battle.net', 'Battle.net.config')
 if (-not (Test-Path $BattleNet_Config)) {
     New-Item -ItemType Directory -Path (Split-Path -Path $BattleNet_Config) -Force
@@ -28,17 +42,3 @@ $BattleNet_JSON = @{
     }
 }
 $BattleNet_JSON | ConvertTo-Json -Depth 10 | Set-Content -Path $BattleNet_Config
-
-$BattleNET_DDL = 'https://downloader.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe'
-$BattleNET_Filename = 'Battle.net-Setup.exe'
-$BattleNET_SavePath = [IO.Path]::Combine($env:TEMP, $BattleNET_Filename)
-[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
-(New-Object System.Net.WebClient).DownloadFile($BattleNET_DDL, $BattleNET_SavePath)
-                
-$BattleNET_Argument = '--lang=enUS --installpath="C:\Program Files (x86)\Battle.net"'
-[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Installing '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_SavePath'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' with '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_Argument'"); [Console]::ResetColor(); [Console]::WriteLine()
-Start-Process $BattleNET_SavePath -ArgumentList $BattleNET_Argument -Wait
-
-if ($null -ne (Get-Item -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run').GetValue('Battle.net')) {
-	Remove-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'Battle.net'
-}
