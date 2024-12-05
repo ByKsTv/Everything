1a28e2e244ba834b09757fc97bdc4ee7037272b4
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-05 23:07:49 +0200

    small fix to net

--- Windows/.NET/Download.ps1
+++ Windows/.NET/Download.ps1
@@ -7,10 +7,13 @@ if (-not (Get-ScheduledTask -TaskName $DotNET_TaskName -ErrorAction SilentlyCont
 	$DotNET_TaskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
 	Register-ScheduledTask -TaskName $DotNET_TaskName -Action $DotNET_TaskAction -Trigger $DotNET_TaskTrigger -Principal $DotNET_TaskPrincipal -Settings $DotNET_TaskSettings -Force
 }
-if (Get-ScheduledTask -TaskName $DotNET_TaskName -ErrorAction SilentlyContinue) {
+
+$Task = Get-ScheduledTask -TaskName $DotNET_TaskName -ErrorAction SilentlyContinue
+if ($Task) {
     $Updated_TaskAction = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument "/C start /MIN powershell -WindowStyle Minimized Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/.NET/Download.ps1')"
     $Updated_TaskPrincipal = New-ScheduledTaskPrincipal -UserId "$env:COMPUTERNAME\$env:USERNAME" -RunLevel Highest
     Register-ScheduledTask -TaskName $DotNET_TaskName -Action $Updated_TaskAction -Trigger $Task.Triggers -Principal $Updated_TaskPrincipal -Settings $Task.Settings -Force
 }
+$Task.Dispose()
 
 # Delete this folder on 01/01/2025
