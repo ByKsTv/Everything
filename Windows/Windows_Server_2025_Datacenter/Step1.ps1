284c1af06e00f08dbe86b0df8b2ea3462f2101e3
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-06 02:23:36 +0200

    remove test

--- Windows/Windows_Server_2025_Datacenter/Step1.ps1
+++ Windows/Windows_Server_2025_Datacenter/Step1.ps1
@@ -1,10 +1,4 @@
-Write-Host "10"
-Start-Sleep -Seconds 10
-
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Key.ps1')
-
-Write-Host "5"
-Start-Sleep -Seconds 5
 
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Network.ps1')
 
