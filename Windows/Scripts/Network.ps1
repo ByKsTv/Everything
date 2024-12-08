ad9791757f75605214fa8953c38a178bb9cf1478
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 12:52:22 +0200

    a

--- Windows/Scripts/Network.ps1
+++ Windows/Scripts/Network.ps1
@@ -1,13 +1,7 @@
 # Private Network
-$FirewallRules = @(
-	# File and printer sharing
-	'@FirewallAPI.dll,-32752',
-
-	# Network discovery
-	'@FirewallAPI.dll,-28502'
-)
-Set-NetFirewallRule -Group $FirewallRules -Profile Private -Enabled True
-Set-NetFirewallRule -Profile Public, Private -Name FPS-SMB-In-TCP -Enabled True
+Set-NetFirewallRule -Group '@FirewallAPI.dll,-28502', '@FirewallAPI.dll,-32752' -Profile Private -Enabled True
+Set-NetFirewallRule -Profile Private -Name FPS-SMB-In-TCP -Enabled True
+Set-NetFirewallRule -Profile Public -Name FPS-SMB-In-TCP -Enabled False
 Set-NetConnectionProfile -NetworkCategory Private
 
 # 1. Retrieve All Network Adapters
