63fc926c45b61425d8f5cbaba07194bfba35ec13
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 07:56:49 +0200

    a

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -751,7 +751,7 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Microsoft_Edge/Uninstall.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['Uninstall Microsoft Store'].Checked) {
-        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Uninstall_Microsoft_Store.ps1')
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Microsoft_Store/Uninstall.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['Valorant'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_Valorant.ps1') 
