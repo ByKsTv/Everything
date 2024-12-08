f9b4d1c400f3bf042a1598a6ad642f74a0c414bb
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 08:02:21 +0200

    a

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -661,7 +661,7 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Display_Driver_Uninstaller/Download.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['Edge WebView2'].Checked) {
-        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_Edge_WebView2.ps1')
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Microsoft_Edge_WebView2/Download.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['eM Client - License Fix'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/eM_Client/License.ps1')
