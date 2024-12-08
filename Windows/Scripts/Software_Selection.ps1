6af98a554590255c1727ed9d18b7a015d107bfc1
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 08:02:55 +0200

    a

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -652,7 +652,7 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/CrystalDiskMark/Download.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['CurseForge'].Checked) {
-        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_CurseForge.ps1')
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/CurseForge/Download.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['Discord'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Discord/Download.ps1')
