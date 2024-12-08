bccdb9bd926f56795d1c2bc4218f9cb5a062d49e
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 08:05:23 +0200

    a

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -625,7 +625,7 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Adobe_Photoshop/Download.ps1') 
     }
     if ($SoftwareSelection_CheckBoxes['AnyDesk'].Checked) {
-        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_AnyDesk.ps1') 
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/AnyDesk/Download.ps1') 
     }
     if ($SoftwareSelection_CheckBoxes['Autodesk AutoCAD'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_Autodesk_AutoCAD.ps1') 
