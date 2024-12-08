5c223a2401d5e03847e87fcce1fd85941ef3f7d9
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 07:58:11 +0200

    a

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -730,7 +730,7 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/qBittorrent/Download.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['Razer Synapse'].Checked) {
-        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_Razer_Synapse.ps1') 
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Razer_Synapse/Download.ps1') 
     }
     if ($SoftwareSelection_CheckBoxes['SketchUp'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_SketchUp.ps1') 
