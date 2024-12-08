b5347738755a638592431593bd93d222fff91da6
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 07:55:08 +0200

    a

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -688,7 +688,7 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_Jellyfin.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['JitBit Macro Recorder'].Checked) {
-        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_JitBit_Macro_Recorder.ps1') 
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Jitbit_Macro_Recorder/Download.ps1') 
     }
     if ($SoftwareSelection_CheckBoxes['Logitech G HUB'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_Logitech_G_HUB.ps1')
