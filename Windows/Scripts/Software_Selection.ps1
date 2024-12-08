c724856ce14c5169435ab8dad88d40ae73d2a4b7
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 08:00:55 +0200

    a

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -682,7 +682,7 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Git/Download.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['HyperX NGENUITY'].Checked) {
-        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_HyperX_NGENUITY.ps1') 
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/HyperX_NGENUITY/Download.ps1') 
     }
     if ($SoftwareSelection_CheckBoxes['Jellyfin'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Jellyfin/Download.ps1')
