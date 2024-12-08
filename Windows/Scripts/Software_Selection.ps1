9e93a9f2bfa8324f922b08407f10460eec2aa1a1
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-08 07:58:44 +0200

    a

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -691,7 +691,7 @@ $SoftwareSelection_Form_OK.Add_Click{
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Jitbit_Macro_Recorder/Download.ps1') 
     }
     if ($SoftwareSelection_CheckBoxes['Logitech G HUB'].Checked) {
-        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Install_Logitech_G_HUB.ps1')
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Logitech_G_HUB/Download.ps1')
     }
     if ($SoftwareSelection_CheckBoxes['MediaInfo'].Checked) {
         Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/MediaInfo/Download.ps1')
