2e841757f9e1004b3749cd371073b9a3bc30d22b
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-09 02:14:40 +0200

    a

--- Windows/Scripts/Network.ps1
+++ Windows/Scripts/Network.ps1
@@ -123,7 +123,7 @@ foreach ($Setting in $DisableAdapterSettings) {
 
 # 8. Enable or Disable Wake-on-LAN Based on User Input
 Add-Type -AssemblyName System.Windows.Forms
-$WakeOnLanAnswer = [Windows.Forms.MessageBox]::Show('Enable Wake-On-Lan?', 'Wake-On-Lan', 4, 32)
+$WakeOnLanAnswer = [Windows.Forms.MessageBox]::Show((New-Object Windows.Forms.Form -Property @{ TopMost = $true }), 'Enable Wake-On-Lan?', 'Wake-On-Lan', 4, 32)
 $PnPValue = if ($WakeOnLanAnswer -eq 'Yes') {
  256 
 }
