aebf680f522750f723644d512a1472b691136fbc
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-10 06:32:30 +0200

    a

--- Windows/Scripts/Build_ISO.ps1
+++ Windows/Scripts/Build_ISO.ps1
@@ -1,4 +1,15 @@
-# Build .ISO from folder
-$path = "path"
-& "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe" -m -o -u2 -udfver102 $path "$path\unattend.iso"
-Write-Host "ISO image created successfully at $isoPath"
+Add-Type -AssemblyName System.Windows.Forms
+
+$FolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+$FolderDialog.ShowNewFolderButton = $true
+
+$form = New-Object System.Windows.Forms.Form
+$form.TopMost = $true
+
+if ($FolderDialog.ShowDialog($form) -eq 'OK') {
+    $path = $FolderDialog.SelectedPath
+
+    & "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe" -m -o -u2 -udfver102 $path "$path\unattend.iso"
+
+    Write-Host "ISO image created successfully at $path\unattend.iso"
+}
--- Windows/Scripts/Extract_Base64.ps1
+++ Windows/Scripts/Extract_Base64.ps1
@@ -1,9 +1,12 @@
 Add-Type -AssemblyName System.Windows.Forms
 
 $FileDialog = New-Object System.Windows.Forms.OpenFileDialog
-$FileDialog.Filter = "All Files (*.*)|*.*"
+$FileDialog.Filter = 'All Files (*.*)|*.*'
 
-if ($FileDialog.ShowDialog() -eq 'OK') {
+$form = New-Object System.Windows.Forms.Form
+$form.TopMost = $true
+
+if ($FileDialog.ShowDialog($form) -eq 'OK') {
     $FileName = $FileDialog.FileName
     $Base64Content = [Convert]::ToBase64String((Get-Content $FileName -Encoding Byte))
 
