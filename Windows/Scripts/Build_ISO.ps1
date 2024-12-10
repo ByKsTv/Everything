eeedacfd00084fa90bda7147bc9818e1824e777d
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-10 13:42:51 +0200

    a

--- Windows/Scripts/Build_ISO.ps1
+++ Windows/Scripts/Build_ISO.ps1
@@ -1,17 +1,25 @@
 Add-Type -AssemblyName System.Windows.Forms
+[Windows.Forms.Application]::EnableVisualStyles()
 
-$FileDialog = New-Object System.Windows.Forms.OpenFileDialog
-$FileDialog.CheckFileExists = $false
-$FileDialog.ValidateNames = $false
-$FileDialog.FileName = 'Select Folder'
+$BuildISO_FileDialog = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+    FileName        = 'Select Folder'
+    CheckFileExists = $false
+    ValidateNames   = $false
+}
+$BuildISO_Form = New-Object System.Windows.Forms.Form -Property @{ 
+    TopMost       = $true
+    ShowInTaskbar = $false
+    Opacity       = 0 
+}
+$BuildISO_Form.Show()
+$BuildISO_OK = $BuildISO_FileDialog.ShowDialog($BuildISO_Form)
+$BuildISO_Form.Dispose()
 
-$form = New-Object System.Windows.Forms.Form
-$form.TopMost = $true
+if ($BuildISO_OK -eq [Windows.Forms.DialogResult]::OK) {
+    $BuildISO_FileSelected = [IO.Path]::GetDirectoryName($BuildISO_FileDialog.FileName)
 
-if ($FileDialog.ShowDialog($form) -eq 'OK') {
-    $path = [IO.Path]::GetDirectoryName($FileDialog.FileName)
-
-    & 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe' -m -o -u2 -udfver102 $path "$path\unattend.iso"
-
-    Write-Host "ISO image created successfully at $path\unattend.iso"
+    & 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe' -m -o -u2 -udfver102 $BuildISO_FileSelected "$BuildISO_FileSelected\File.iso"
+}
+else {
+    Write-Output 'No file selected.'
 }
