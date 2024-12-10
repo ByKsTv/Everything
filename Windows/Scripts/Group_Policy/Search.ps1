d7040b2ce9f220ba523f37209ab117347116461d
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-10 07:16:15 +0200

    a

--- Windows/Scripts/Group_Policy/Search.ps1
+++ Windows/Scripts/Group_Policy/Search.ps1
@@ -1,14 +1,52 @@
-$SearchDirectory = "$env:windir\PolicyDefinitions"
-$SearchText = '*AllowAutoWindowsUpdateDownloadOverMeteredNetwork*'
-$Files = Get-ChildItem -Path $SearchDirectory -Recurse -File
-foreach ($File in $Files) {
-    try {
-        $FileContent = Get-Content -Path $File.FullName -ErrorAction Stop
-        if ($FileContent -like $SearchText) {
-            Write-Host "Found '$SearchText' in file: $($File.FullName)" -ForegroundColor Green
+Add-Type -AssemblyName System.Windows.Forms
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Search File Content"
+$form.TopMost = $true
+$form.StartPosition = "CenterScreen"
+$form.Width = 400
+$form.Height = 150
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Width = 350
+$textBox.Top = 20
+$textBox.Left = 20
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Text = "OK"
+$okButton.Top = 60
+$okButton.Left = 100
+$okButton.Width = 80
+$okButton.Add_Click({
+    $form.Tag = $textBox.Text
+    $form.Close()
+})
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Text = "Cancel"
+$cancelButton.Top = 60
+$cancelButton.Left = 200
+$cancelButton.Width = 80
+$cancelButton.Add_Click({
+    $form.Tag = $null
+    $form.Close()
+})
+
+$form.Controls.Add($textBox)
+$form.Controls.Add($okButton)
+$form.Controls.Add($cancelButton)
+$form.ShowDialog()
+
+$SearchText = $form.Tag
+if (![string]::IsNullOrWhiteSpace($SearchText)) {
+    $SearchText = "*$SearchText*"
+    $Files = Get-ChildItem -Path "$env:windir\PolicyDefinitions" -Recurse -File
+    foreach ($File in $Files) {
+        try {
+            if ((Get-Content $File.FullName -ErrorAction Stop) -like $SearchText) {
+                Write-Host "Found '$SearchText' in: $($File.FullName)" -ForegroundColor Green
+            }
+        } catch {
+            Write-Host "Error reading: $($File.FullName)" -ForegroundColor Red
         }
-    }
-    catch {
-        Write-Host "Error reading file: $($File.FullName)" -ForegroundColor Red
     }
 }
