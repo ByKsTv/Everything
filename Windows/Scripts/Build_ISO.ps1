73dc426e995f3141d416c6429af266813844fb6b
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-16 06:29:51 +0200

    a

--- Windows/Scripts/Build_ISO.ps1
+++ Windows/Scripts/Build_ISO.ps1
@@ -13,6 +13,11 @@ $BuildISO_FileDialog = New-Object System.Windows.Forms.OpenFileDialog -Property 
 if ($BuildISO_FileDialog.ShowDialog($BuildISO_Form) -eq [Windows.Forms.DialogResult]::OK) {
     $BuildISO_FileSelected = [IO.Path]::GetDirectoryName($BuildISO_FileDialog.FileName)
 
+    $InstalledSoftware = (Get-Package).Name
+    if ($InstalledSoftware -notmatch 'Windows System Image Manager') {
+        Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Windows_Assessment_and_Deployment_Kit/Deployment_Tools/Download.ps1')
+    }
+
     & 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe' -m -o -u2 -udfver102 $BuildISO_FileSelected "$BuildISO_FileSelected\ISO.iso"
 }
 
