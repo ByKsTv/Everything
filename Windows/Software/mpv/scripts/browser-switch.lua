2d7748b166e4a5b5f36421624031bff68a68edd4
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-19 16:31:30 +0200

    refractor browser switch

--- Windows/Software/mpv/scripts/browser-switch.lua
+++ Windows/Software/mpv/scripts/browser-switch.lua
@@ -1,20 +1,19 @@
-local function read_time1(_, time_pos)
-    if time_pos and time_pos > mp.get_property_native('duration', math.huge) - 1 then
-        local url = mp.get_property("path")
-        if string.match(url, "http") then
-            if mp.get_property_number('playlist-count') == 1 then
-                mp.command_native({
-                    name = 'subprocess',
-                    capture_stdout = true,
-                    args = {'powershell', '-NoProfile', '-Command',
-                            '$browser = Get-Process -Name chrome, firefox -ErrorAction SilentlyContinue | Where-Object { $_.MainWindowTitle } | Select-Object -First 1; if ($browser) { (New-Object -ComObject WScript.Shell).AppActivate($browser.Id); }'}
-                })
-                mp.unobserve_property(read_time1)
-            end
-        end
+local TIME_THRESHOLD = 1
+
+local function browser_switch(_, remaining_time)
+    if remaining_time and remaining_time <= TIME_THRESHOLD and mp.get_property("path", ""):match("^https?://") and
+        mp.get_property_number('playlist-count') == 1 then
+        mp.command_native({
+            name = 'subprocess',
+            capture_stdout = true,
+            args = {'powershell', '-NoProfile', '-Command',
+                    '$browser = Get-Process -Name chrome, firefox -ErrorAction SilentlyContinue | Where-Object { $_.MainWindowTitle } | Select-Object -First 1; if ($browser) { (New-Object -ComObject WScript.Shell).AppActivate($browser.Id); }'}
+        })
+        print(TIME_THRESHOLD .. " seconds remaining - switching to browser")
+        mp.unobserve_property(browser_switch)
     end
 end
 
-mp.register_event('file-loaded', function()
-    mp.observe_property('time-pos', 'native', read_time1)
+mp.register_event("file-loaded", function()
+    mp.observe_property("playtime-remaining", "native", browser_switch)
 end)
