eae95562dea04078fcfd6e6b768b3c32443e324b
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-19 16:31:56 +0200

    Windows/Software/mpv/scripts/delete_file_auto.lua: refractor

--- Windows/Software/mpv/scripts/delete_file_auto.lua
+++ Windows/Software/mpv/scripts/delete_file_auto.lua
@@ -1,10 +1,13 @@
-local function read_time(_, time_pos)
-    if time_pos and time_pos > mp.get_property_native('duration', math.huge) - 15 then
-        mp.command('script-binding delete_file/delete_file')
-        mp.unobserve_property(read_time)
+local TIME_THRESHOLD = 15
+
+local function delete_file_auto(_, remaining_time)
+    if remaining_time and remaining_time <= TIME_THRESHOLD and not mp.get_property("path", ""):match("^https?://") then
+        print(TIME_THRESHOLD .. " seconds remaining - auto deleting current file")
+        mp.command("script-binding delete_file/delete_file")
+        mp.unobserve_property(delete_file_auto)
     end
 end
 
-mp.register_event('file-loaded', function()
-    mp.observe_property('time-pos', 'native', read_time)
+mp.register_event("file-loaded", function()
+    mp.observe_property("playtime-remaining", "native", delete_file_auto)
 end)
