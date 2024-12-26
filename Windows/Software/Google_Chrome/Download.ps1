8c6f5c76461440a59550937986fcce7980305ca3
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2024-12-26 22:25:09 +0200

    Windows/Software/Google_Chrome/Download.ps1: edit popup manual

--- Windows/Software/Google_Chrome/Download.ps1
+++ Windows/Software/Google_Chrome/Download.ps1
@@ -35,7 +35,9 @@ Start-Process -FilePath $Chrome_Installer_SavePath
 Start-Process 'ms-settings:defaultapps'
 
 Add-Type -AssemblyName System.Windows.Forms
-$Popup_Usermanual = New-Object System.Windows.Forms.Form -Property @{TopMost = $true; ShowInTaskbar = $false; Opacity = 0 }
+$Popup_Usermanual = New-Object System.Windows.Forms.Form -Property @{
+    TopMost = $true
+}
 $Popup_Text = "Please set 'Google Chrome' as default web browser"
 [Windows.Forms.MessageBox]::Show($Popup_Usermanual, $Popup_Text, '', 'OK') | Out-Null
 $Popup_Usermanual.Dispose()
