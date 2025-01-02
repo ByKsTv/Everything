770091bd4a1f67641e72925ee8268ba0331b7521
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-01-02 22:19:19 +0200

    Windows/Scripts/Settings.ps1: format

--- Windows/Scripts/Settings.ps1
+++ Windows/Scripts/Settings.ps1
@@ -550,18 +550,18 @@ New-ItemProperty -Path 'HKLM:\System\ControlSet001\Control\PriorityControl' -Nam
 
 $PciDevicesPath = 'HKLM:\SYSTEM\CurrentControlSet\Enum\PCI'
 Get-ChildItem -Path $PciDevicesPath -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | ForEach-Object {
-    Get-ChildItem -Path $_.PSPath -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | ForEach-Object {
-        $InterruptManagementPath = Join-Path -Path $_.PSPath -ChildPath 'Device Parameters\Interrupt Management'
-        $DeviceParametersPath = Join-Path -Path $InterruptManagementPath -ChildPath 'MessageSignaledInterruptProperties'
-        $AffinityPolicyPath = Join-Path -Path $InterruptManagementPath -ChildPath 'Affinity Policy'
+	Get-ChildItem -Path $_.PSPath -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | ForEach-Object {
+		$InterruptManagementPath = Join-Path -Path $_.PSPath -ChildPath 'Device Parameters\Interrupt Management'
+		$DeviceParametersPath = Join-Path -Path $InterruptManagementPath -ChildPath 'MessageSignaledInterruptProperties'
+		$AffinityPolicyPath = Join-Path -Path $InterruptManagementPath -ChildPath 'Affinity Policy'
 
-        @($InterruptManagementPath, $DeviceParametersPath, $AffinityPolicyPath) | ForEach-Object {
-            if (-not (Test-Path $_)) {
-                New-Item -Path $_ -ItemType Directory -Force
-            }
-        }
+		@($InterruptManagementPath, $DeviceParametersPath, $AffinityPolicyPath) | ForEach-Object {
+			if (-not (Test-Path $_)) {
+				New-Item -Path $_ -ItemType Directory -Force
+			}
+		}
 
-        New-ItemProperty -Path $DeviceParametersPath -Name 'MSISupported' -Value 1 -PropertyType DWord -Force
-        New-ItemProperty -Path $AffinityPolicyPath -Name 'DevicePriority' -Value 3 -PropertyType DWord -Force
-    }
+		New-ItemProperty -Path $DeviceParametersPath -Name 'MSISupported' -Value 1 -PropertyType DWord -Force
+		New-ItemProperty -Path $AffinityPolicyPath -Name 'DevicePriority' -Value 3 -PropertyType DWord -Force
+	}
 }
