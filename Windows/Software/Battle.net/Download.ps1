0aa685d55a9cf7edcd623c6ab28e4e1e29161a58
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-01-04 16:14:25 +0200

    Windows/Software/Battle.net/Download.ps1: download overwatch settings

--- Windows/Software/Battle.net/Download.ps1
+++ Windows/Software/Battle.net/Download.ps1
@@ -1,3 +1,15 @@
+$OverWatch_RemoteSettings_DDL = "https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Battle.net/Settings_v0.ini"
+$OverWatch_RemoteSettings_Filename = [IO.Path]::GetFileName(([URI]$OverWatch_RemoteSettings_DDL).AbsolutePath)
+$OverWatch_LocalSettings_Dir = "$HOME\Documents\Overwatch\Settings"
+$OverWatch_LocalSettings_SavePath = [IO.Path]::Combine($OverWatch_LocalSettings_Dir, $OverWatch_RemoteSettings_Filename)
+
+if (-not (Test-Path $OverWatch_LocalSettings_SavePath)) {
+    New-Item -Path $OverWatch_LocalSettings_SavePath -ItemType File -Force
+
+    [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Overwatch'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' settings from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$OverWatch_RemoteSettings_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$OverWatch_LocalSettings_SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
+    (New-Object System.Net.WebClient).DownloadFile($OverWatch_RemoteSettings_DDL, $OverWatch_LocalSettings_SavePath)
+}
+
 $BattleNET_DDL = 'https://downloader.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe'
 $BattleNET_Filename = 'Battle.net-Setup.exe'
 $BattleNET_SavePath = [IO.Path]::Combine($env:TEMP, $BattleNET_Filename)
