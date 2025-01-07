44636d960debf310c32b3dcd7b33363168e2cd21
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-01-07 09:53:38 +0200

    Windows/Software/Battle.net/Download.ps1: download battle.net settings

--- Windows/Software/Battle.net/Download.ps1
+++ Windows/Software/Battle.net/Download.ps1
@@ -1,4 +1,4 @@
-$OverWatch_RemoteSettings_DDL = "https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Battle.net/Settings_v0.ini"
+$OverWatch_RemoteSettings_DDL = 'https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Battle.net/Settings_v0.ini'
 $OverWatch_RemoteSettings_Filename = [IO.Path]::GetFileName(([URI]$OverWatch_RemoteSettings_DDL).AbsolutePath)
 $OverWatch_LocalSettings_Dir = "$HOME\Documents\Overwatch\Settings"
 $OverWatch_LocalSettings_SavePath = [IO.Path]::Combine($OverWatch_LocalSettings_Dir, $OverWatch_RemoteSettings_Filename)
@@ -9,6 +9,17 @@ if (-not (Test-Path $OverWatch_LocalSettings_SavePath)) {
     [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Overwatch'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' settings from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$OverWatch_RemoteSettings_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$OverWatch_LocalSettings_SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
     (New-Object System.Net.WebClient).DownloadFile($OverWatch_RemoteSettings_DDL, $OverWatch_LocalSettings_SavePath)
 }
+
+$BattleNET_AppData_Dir = "$env:APPDATA\Battle.net"
+if (-not (Test-Path $BattleNET_AppData_Dir)) {
+    New-Item -Path $BattleNET_AppData_Dir -ItemType Directory -Force
+}
+
+$BattleNET_AppData_DDL = 'https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Battle.net/Battle.net.config'
+$BattleNET_AppData_Filename = [IO.Path]::GetFileName(([URI]$BattleNET_AppData_DDL).AbsolutePath)
+$BattleNET_AppData_SavePath = [IO.Path]::Combine($BattleNET_AppData_Dir, $BattleNET_AppData_Filename)
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' settings from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_AppData_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_AppData_SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
+(New-Object System.Net.WebClient).DownloadFile($BattleNET_AppData_DDL, $BattleNET_AppData_SavePath)
 
 $BattleNET_DDL = 'https://downloader.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe'
 $BattleNET_Filename = 'Battle.net-Setup.exe'
