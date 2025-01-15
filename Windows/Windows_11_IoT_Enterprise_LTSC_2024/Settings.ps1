9e5d3f0ad449e7812ac763457c30202965098a23
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-01-15 09:38:50 +0200

    Windows/Windows_11_IoT_Enterprise_LTSC_2024/Settings.ps1: only apply whats needed

--- Windows/Windows_11_IoT_Enterprise_LTSC_2024/Settings.ps1
+++ Windows/Windows_11_IoT_Enterprise_LTSC_2024/Settings.ps1
@@ -1,1 +1,5 @@
-# TODO
+# Remove 'Get Started' app
+# Remove 'Windows Backup' app
+
+# Administrative Shares: Disable
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters' -Name 'AutoShareWks' -Value '0' -PropertyType DWord -Force
