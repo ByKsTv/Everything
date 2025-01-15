2c8395ecaf9db09a8a5c810c68ab9318dbcddd61
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-01-15 09:39:57 +0200

    Windows/Windows_Server_2025_Datacenter/Step1.ps1: add Settings_Windows11

--- Windows/Windows_Server_2025_Datacenter/Step1.ps1
+++ Windows/Windows_Server_2025_Datacenter/Step1.ps1
@@ -2,6 +2,8 @@ Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubu
 
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Settings.ps1')
 
+Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Settings_Windows11.ps1')
+
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Windows_Server_2025_Datacenter/Settings.ps1')
 
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/O&O_ShutUp10++.ps1')
