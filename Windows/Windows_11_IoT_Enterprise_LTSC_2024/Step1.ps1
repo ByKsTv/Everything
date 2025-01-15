41c982fbbf3e32f735bd408fc625b6d4851c0592
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-01-15 17:45:23 +0200

    reorder settings

--- Windows/Windows_11_IoT_Enterprise_LTSC_2024/Step1.ps1
+++ Windows/Windows_11_IoT_Enterprise_LTSC_2024/Step1.ps1
@@ -1,8 +1,8 @@
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Key.ps1')
 
-Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Settings_Windows11.ps1')
-
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Windows_11_IoT_Enterprise_LTSC_2024/Settings.ps1')
+
+Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Settings_Windows11.ps1')
 
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Settings.ps1')
 
