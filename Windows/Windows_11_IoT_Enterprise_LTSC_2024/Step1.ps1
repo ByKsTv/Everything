16fe9b87fa6fcfba6ad8ffcdbd219c07e5eeb167
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-03-17 00:32:48 +0200

    Windows/Windows_11_IoT_Enterprise_LTSC_2024/Step1.ps1: improve downloading step2

--- Windows/Windows_11_IoT_Enterprise_LTSC_2024/Step1.ps1
+++ Windows/Windows_11_IoT_Enterprise_LTSC_2024/Step1.ps1
@@ -1,7 +1,6 @@
 $NextStep_TaskName = 'Step2'
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Windows_11_IoT_Enterprise_LTSC_2024/$NextStep_TaskName.ps1", "$env:TEMP\$NextStep_TaskName.ps1")
 [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Task Scheduler: Adding '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$NextStep_TaskName'"); [Console]::ResetColor(); [Console]::WriteLine()
-$NextStep_TaskAction = New-ScheduledTaskAction -Execute powershell.exe -Argument "-WindowStyle Maximized -ExecutionPolicy Bypass -File $env:TEMP\$NextStep_TaskName.ps1"
+$NextStep_TaskAction = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument "/C start /MIN powershell -WindowStyle Minimized -Command `"`$Host.UI.RawUI.WindowTitle = '$NextStep_TaskName'; while (!(Resolve-DnsName google.com -ErrorAction SilentlyContinue)) { Start-Sleep -Seconds 1 }; Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Windows_11_IoT_Enterprise_LTSC_2024/$NextStep_TaskName.ps1')`""
 $NextStep_TaskTrigger = New-ScheduledTaskTrigger -AtLogOn
 $NextStep_TaskPrincipal = New-ScheduledTaskPrincipal -UserId "$env:computername\$env:USERNAME" -RunLevel Highest
 $NextStep_TaskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
