153d01196f3fbd15807f6636b52c2d5f0c2503b6
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-03-17 00:27:05 +0200

    Windows/Software/Mozilla_Firefox/Arkenfox.ps1: update to use `Start-Process` and add `-Wait`

--- Windows/Software/Mozilla_Firefox/Arkenfox.ps1
+++ Windows/Software/Mozilla_Firefox/Arkenfox.ps1
@@ -41,9 +41,9 @@ if (Test-Path $Firefox_Profiles) {
             (New-Object Net.WebClient).DownloadFile($Firefox_ScriptURL, $Firefox_ScriptSavePath)
         }
         
-        & "$Firefox_Profile\updater.bat" -unattended -updatebatch
+        Start-Process -FilePath "$Firefox_Profile\updater.bat" -ArgumentList "-unattended", "-updatebatch" -Wait
 
-        & "$Firefox_Profile\prefsCleaner.bat" -unattended
+        Start-Process -FilePath "$Firefox_Profile\prefsCleaner.bat" -ArgumentList "-unattended" -Wait
 
         $Firefox_DirsToDelete = 'datareporting', 'crashes', 'saved-telemetry-pings', 'minidumps'
         foreach ($Firefox_DirToDelete in $Firefox_DirsToDelete) {
