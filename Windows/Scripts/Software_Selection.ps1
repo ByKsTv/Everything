96a27e34f7ffeb44943a4cf238161f30b46a96e1
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-03-17 05:19:52 +0200

    Windows/Scripts/Software_Selection.ps1: allow to reinstall chrome exts even while already installed

--- Windows/Scripts/Software_Selection.ps1
+++ Windows/Scripts/Software_Selection.ps1
@@ -427,7 +427,6 @@ if ($InstalledSoftware -match 'Google Chrome') {
     $SoftwareSelection_CheckBoxes['Chrome'].Text += ' (Installed)'
 }
 if ((Test-Path -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist') -eq $true) {
-    $SoftwareSelection_CheckBoxes['Chrome - Extensions'].Enabled = $false
     $SoftwareSelection_CheckBoxes['Chrome - Extensions'].Text += ' (Installed)'
 }
 if ($InstalledSoftware -match 'CrystalDiskInfo') {
