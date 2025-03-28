cd0a0bc1b060438cc4e488f9d5a7ff10a15ec9ec
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-03-29 02:05:44 +0300

    Windows/Software/Mozilla_Firefox/user-overrides.js: update

--- Windows/Software/Mozilla_Firefox/user-overrides.js
+++ Windows/Software/Mozilla_Firefox/user-overrides.js
@@ -96,7 +96,7 @@ user_pref("identity.fxaccounts.toolbar.enabled", false);
 
 // Enable media autoplay
 user_pref("media.autoplay.blocking_policy", 0);
-user_pref("media.autoplay.default", 0);
+user_pref("media.autoplay.default", 1);
 
 // Maximize Window
 user_pref("privacy.resistFingerprinting", false);
