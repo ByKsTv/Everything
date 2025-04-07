4f8e65c86234032f4ac479c401f73968452e65ab
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-04-07 18:02:34 +0300

    Windows/Scripts/Settings.ps1: Enable MSI and High Priority

--- Windows/Scripts/Settings.ps1
+++ Windows/Scripts/Settings.ps1
@@ -605,23 +605,23 @@ New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer
 New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\PriorityControl' -Name 'Win32PrioritySeparation' -Value 38 -PropertyType DWord -Force
 
 # Enable MSI and High Priority
-# $PciDevicesPath = 'HKLM:\SYSTEM\CurrentControlSet\Enum\PCI'
-# Get-ChildItem -Path $PciDevicesPath -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | ForEach-Object {
-# 	Get-ChildItem -Path $_.PSPath -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | ForEach-Object {
-# 		$InterruptManagementPath = Join-Path -Path $_.PSPath -ChildPath 'Device Parameters\Interrupt Management'
-# 		$DeviceParametersPath = Join-Path -Path $InterruptManagementPath -ChildPath 'MessageSignaledInterruptProperties'
-# 		$AffinityPolicyPath = Join-Path -Path $InterruptManagementPath -ChildPath 'Affinity Policy'
+$PciDevicesPath = 'HKLM:\SYSTEM\CurrentControlSet\Enum\PCI'
+Get-ChildItem -Path $PciDevicesPath -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | ForEach-Object {
+	Get-ChildItem -Path $_.PSPath -ErrorAction SilentlyContinue | Where-Object { $_.PSIsContainer } | ForEach-Object {
+		$InterruptManagementPath = Join-Path -Path $_.PSPath -ChildPath 'Device Parameters\Interrupt Management'
+		$DeviceParametersPath = Join-Path -Path $InterruptManagementPath -ChildPath 'MessageSignaledInterruptProperties'
+		$AffinityPolicyPath = Join-Path -Path $InterruptManagementPath -ChildPath 'Affinity Policy'
 
-# 		@($InterruptManagementPath, $DeviceParametersPath, $AffinityPolicyPath) | ForEach-Object {
-# 			if (-not (Test-Path $_)) {
-# 				New-Item -Path $_ -ItemType Directory -Force
-# 			}
-# 		}
+		@($InterruptManagementPath, $DeviceParametersPath, $AffinityPolicyPath) | ForEach-Object {
+			if (-not (Test-Path $_)) {
+				New-Item -Path $_ -ItemType Directory -Force
+			}
+		}
 
-# 		New-ItemProperty -Path $DeviceParametersPath -Name 'MSISupported' -Value 1 -PropertyType DWord -Force
-# 		New-ItemProperty -Path $AffinityPolicyPath -Name 'DevicePriority' -Value 3 -PropertyType DWord -Force
-# 	}
-# }
+		New-ItemProperty -Path $DeviceParametersPath -Name 'MSISupported' -Value 1 -PropertyType DWord -Force
+		New-ItemProperty -Path $AffinityPolicyPath -Name 'DevicePriority' -Value 3 -PropertyType DWord -Force
+	}
+}
 
 # Control Panel: Ease of Access: Ease of Access Center: Always read this section aloud: off
 if (-not (Test-Path -Path 'HKCU:\SOFTWARE\Microsoft\Ease of Access')) {
