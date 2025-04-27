15ba5665f8cff453f9132d1361f4b4112a8649ca
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-04-27 12:26:50 +0300

    Windows/Software/Battle.net/User.ps1: fix typo

--- Windows/Software/Battle.net/User.ps1
+++ Windows/Software/Battle.net/User.ps1
@@ -1,4 +1,4 @@
-[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Waiting for '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'User'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to login to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' account '); [Console]::ResetColor(); [Console]::WriteLine()
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Waiting for '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'User'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to login to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' account'); [Console]::ResetColor(); [Console]::WriteLine()
 
 do {
     $BattleNET_UserLogin_File = (Get-ChildItem "$env:AppData\Battle.net" -Filter *.config | Where-Object { $_.Name -ne 'battle.net.config' }).Name
