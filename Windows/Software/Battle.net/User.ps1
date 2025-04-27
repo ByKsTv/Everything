dc7fe60527a1893fbe5a1b70ab5699077845cdb2
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-04-27 18:26:18 +0300

    battle.net format as json

--- Windows/Software/Battle.net/User.ps1
+++ Windows/Software/Battle.net/User.ps1
@@ -7,7 +7,7 @@ do {
 
 Get-Process -Name 'Battle.net' -ErrorAction SilentlyContinue | Stop-Process -Force
 
-$BattleNET_UserLogin_Settings_DDL = 'https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Battle.net/User.config'
+$BattleNET_UserLogin_Settings_DDL = 'https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Battle.net/User.json'
 $BattleNET_UserLogin_Settings_Path = [IO.Path]::Combine("$env:AppData\Battle.net", $BattleNET_UserLogin_File)
 [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' settings from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_UserLogin_Settings_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_UserLogin_Settings_Path'"); [Console]::ResetColor(); [Console]::WriteLine()
 (New-Object System.Net.WebClient).DownloadFile($BattleNET_UserLogin_Settings_DDL, $BattleNET_UserLogin_Settings_Path)
