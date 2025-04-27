c402424555accce6ad347b1057711be3374122ba
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-04-27 12:20:46 +0300

    Windows/Software/Battle.net/User.ps1: upload file

---
+++ Windows/Software/Battle.net/User.ps1
@@ -0,0 +1,13 @@
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Waiting for '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'User'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to login to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' account '); [Console]::ResetColor(); [Console]::WriteLine()
+
+do {
+    $BattleNET_UserLogin_File = (Get-ChildItem "$env:AppData\Battle.net" -Filter *.config | Where-Object { $_.Name -ne 'battle.net.config' }).Name
+    Start-Sleep 1
+} until ($BattleNET_UserLogin_File)
+
+Get-Process -Name 'Battle.net' -ErrorAction SilentlyContinue | Stop-Process -Force
+
+$BattleNET_UserLogin_Settings_DDL = 'https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Software/Battle.net/User.config'
+$BattleNET_UserLogin_Settings_Path = [IO.Path]::Combine("$env:AppData\Battle.net", $BattleNET_UserLogin_File)
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Battle.net'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' settings from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_UserLogin_Settings_DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$BattleNET_UserLogin_Settings_Path'"); [Console]::ResetColor(); [Console]::WriteLine()
+(New-Object System.Net.WebClient).DownloadFile($BattleNET_UserLogin_Settings_DDL, $BattleNET_UserLogin_Settings_Path)
