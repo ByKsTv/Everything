1059872e1bfca8628e12ccbe9239e602abc73784
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-07-18 09:34:21 +0300

    Windows/Software/CrystalDiskMark/Download.ps1: fix

--- Windows/Software/CrystalDiskMark/Download.ps1
+++ Windows/Software/CrystalDiskMark/Download.ps1
@@ -1,7 +1,8 @@
 [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Software Selection: CrystalDiskMark: Downloading'); [Console]::ResetColor(); [Console]::WriteLine()
+$DDL = [Uri]::UnescapeDataString((Invoke-WebRequest -UseBasicParsing -Uri 'https://crystalmark.info/redirect.php?product=CrystalDiskMarkInstaller' -MaximumRedirection 0 -ErrorAction SilentlyContinue).Headers.Location) -replace '.*ad_redirect\.php\?url=', ''
 $CrystalDiskMark = New-Object System.Net.WebClient
 $CrystalDiskMark.Headers.Add('user-agent', 'Wget')
-$CrystalDiskMark.DownloadFile('https://sourceforge.net/projects/crystaldiskmark/files/latest/download', "$env:temp\CrystalDiskMark.exe")
+$CrystalDiskMark.DownloadFile($DDL, "$env:temp\CrystalDiskMark.exe")
 
 [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Software Selection: CrystalDiskMark: Installing'); [Console]::ResetColor(); [Console]::WriteLine()
 Start-Process $env:temp\CrystalDiskMark.exe -ArgumentList '/VERYSILENT'
