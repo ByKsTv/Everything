396dc807c6f6a9b4d252e4caff5154fc101c37eb
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-07-18 09:34:15 +0300

    Windows/Software/CrystalDiskInfo/Download.ps1: fix

--- Windows/Software/CrystalDiskInfo/Download.ps1
+++ Windows/Software/CrystalDiskInfo/Download.ps1
@@ -1,7 +1,8 @@
 [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Software Selection: CrystalDiskInfo: Downloading'); [Console]::ResetColor(); [Console]::WriteLine()
+$DDL = [Uri]::UnescapeDataString((Invoke-WebRequest -UseBasicParsing -Uri 'https://crystalmark.info/redirect.php?product=CrystalDiskInfoInstaller' -MaximumRedirection 0 -ErrorAction SilentlyContinue).Headers.Location) -replace '.*ad_redirect\.php\?url=', ''
 $CrystalDiskInfo = New-Object System.Net.WebClient
 $CrystalDiskInfo.Headers.Add('user-agent', 'Wget')
-$CrystalDiskInfo.DownloadFile('https://sourceforge.net/projects/crystaldiskinfo/files/latest/download', "$env:temp\CrystalDiskInfo.exe")
+$CrystalDiskInfo.DownloadFile($DDL, "$env:temp\CrystalDiskInfo.exe")
 
 [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Software Selection: CrystalDiskInfo: Installing'); [Console]::ResetColor(); [Console]::WriteLine()
 Start-Process $env:temp\CrystalDiskInfo.exe -ArgumentList '/VERYSILENT'
