eda4e0ed5cdfdf99172cf90ea0c106d87811abcb
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-08-19 16:19:11 +0300

    Windows/Software/Microsoft_Edge_WebView2/Download.ps1: remove scheduled task because user needs it

--- Windows/Software/Microsoft_Edge_WebView2/Download.ps1
+++ Windows/Software/Microsoft_Edge_WebView2/Download.ps1
@@ -1,3 +1,8 @@
+$TaskName = 'Edge Uninstaller'
+if (Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
+}
+
 $DDL = (Invoke-WebRequest -UseBasicParsing -Uri 'https://go.microsoft.com/fwlink/p/?LinkId=2124703' -MaximumRedirection 0 -ErrorAction SilentlyContinue).Headers.Location
 $FileName = [IO.Path]::GetFileName(([URI]$DDL).AbsolutePath)
 $SavePath = [IO.Path]::Combine($env:TEMP, $FileName)
