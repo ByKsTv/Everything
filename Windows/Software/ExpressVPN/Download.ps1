30e665927ab444469075a9f1795ca6549526c4a0
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-09-28 01:39:13 +0300

    Windows/Software/ExpressVPN/Download.ps1: upload

---
+++ Windows/Software/ExpressVPN/Download.ps1
@@ -0,0 +1,11 @@
+$DDL = ((Invoke-WebRequest -UseBasicParsing -Uri 'https://www.expressvpn.com/vpn-download/vpn-windows').Links | Where-Object { $_.outerHTML -match '.exe' -and $_.outerHTML -notmatch 'arm64' }).href
+$FileName = [IO.Path]::GetFileName(([URI]$DDL).AbsolutePath)
+$SavePath = [IO.Path]::Combine($env:TEMP, $FileName)
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'ExpressVPN'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
+$ExpressVPN = New-Object System.Net.WebClient
+$ExpressVPN.Headers.Add('user-agent', 'Wget')
+$ExpressVPN.DownloadFile($DDL, $SavePath)
+
+$Argument = '/install /quiet /norestart'
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Installing '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'ExpressVPN'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$SavePath'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' with '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$Argument'"); [Console]::ResetColor(); [Console]::WriteLine()
+Start-Process $SavePath -ArgumentList $Argument # Do not add `-Wait`
