47cc4f98afbc093fc88cdd42c4076e0aa3afdec0
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-10-04 09:15:44 +0300

    Windows/Scripts/Amazon_CleanURL.user.js: details

--- Windows/Scripts/Amazon_CleanURL.user.js
+++ Windows/Scripts/Amazon_CleanURL.user.js
@@ -1,8 +1,11 @@
 // ==UserScript==
-// @name         Amazon → /dp/ canonical
-// @match        https://*.amazon.*/*
-// @run-at       document-start
+// @name            Amazon - Shorten URL
+// @version         1.0
+// @match           https://*.amazon.*/*
+// @downloadURL     https://raw.githubusercontent.com/ByKsTv/Everything/main/Windows/Scripts/Amazon_CleanURL.user.js
+// @run-at          document-start
 // ==/UserScript==
+
 (function () {
   const m = location.pathname.match(/(?:dp|gp\/product)\/([A-Z0-9]{10})/i);
   if (m && (location.pathname !== `/dp/${m[1]}` || location.search)) {
