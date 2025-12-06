f93bce04f71d4b9a72dee33254ab39bb2bb1bd2c
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-12-06 18:54:56 +0200

    update

--- Windows/Software/VMware_Workstation/Download.ps1
+++ Windows/Software/VMware_Workstation/Download.ps1
@@ -87,7 +87,7 @@ if ($Form.ShowDialog() -eq [Windows.Forms.DialogResult]::OK) {
     }
     
     [Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Adding '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$env:TEMP'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'Microsoft Defender Exclusions'"); [Console]::ResetColor(); [Console]::WriteLine()
-    Add-MpPreference -ExclusionPath $env:TEMP
+    Add-MpPreference -ExclusionPath $TempDir
     
     while (-not ($TempEXE = (Get-ChildItem $TempDir -Filter '*.exe' | Select-Object -First 1).FullName)) {
         Start-Sleep -Milliseconds 1000
