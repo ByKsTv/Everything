9a4310026467776c4e36fc40388fca1b83dd5f49
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2025-12-15 08:01:43 +0200

    update

---
+++ Windows/Software/GOG_Galaxy/Download.ps1
@@ -0,0 +1,8 @@
+$DDL = ((Invoke-WebRequest -UseBasicParsing -Uri 'https://www.gog.com/galaxy').Links | Where-Object { $_.outerHTML -match '.exe' }).href | Select-Object -First 1
+$FileName = [IO.Path]::GetFileName(([URI]$DDL).AbsolutePath)
+$SavePath = [IO.Path]::Combine($env:TEMP, $FileName)
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Downloading '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'GOG GALAXY'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$DDL'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' to '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$SavePath'"); [Console]::ResetColor(); [Console]::WriteLine()
+(New-Object System.Net.WebClient).DownloadFile($DDL, $SavePath)
+
+[Console]::BackgroundColor = 'Black'; [Console]::ForegroundColor = 'Green'; [Console]::Write('Installing '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$FileName'"); [Console]::ForegroundColor = 'Green'; [Console]::Write(' from '); [Console]::ForegroundColor = 'Yellow'; [Console]::Write("'$SavePath'"); [Console]::ForegroundColor = 'Green'; [Console]::ResetColor(); [Console]::WriteLine()
+Start-Process -FilePath $SavePath
