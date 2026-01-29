52569a40d114369d439d20fcd6dc306f22499b5c
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2026-01-29 16:03:43 +0200

    format

--- Windows/Software/qBittorrent/Theme/make-resource.py
+++ Windows/Software/qBittorrent/Theme/make-resource.py
@@ -6,6 +6,7 @@ import sys
 import fnmatch
 import re
 
+
 def allFiles(glob):
     [dir, pattern] = os.path.split(glob)
     l = list()
@@ -13,34 +14,62 @@ def allFiles(glob):
         l.extend([os.path.join(root, x) for x in files if fnmatch.fnmatch(x, pattern)])
     return l
 
-parser = argparse.ArgumentParser(description='helper to create qbtthemes')
-parser.add_argument('-output', type=str, help='output qbtheme file', default='style.qbtheme')
-parser.add_argument('-style', type=str, help='stylesheet', required=True)
-parser.add_argument('-base-dir', type=str, dest='baseDir', default='.')
-parser.add_argument('-icons-dir', type=str, dest='iconsDir', default='', help='directory which contains custom icons')
-parser.add_argument('-dir-prefix', type=str, default='', dest='dirPrefix', help='prefix added to all files')
-parser.add_argument('-config', type=str, dest='config', default=None, help='file used as config.json')
-parser.add_argument('-find-files', action='store_true', dest='findFiles', help='find files included in qss and only include those')
-parser.add_argument('files', metavar='files', type=str,
-                    nargs='*', default=['*'], help='files to include in resources from baseDir, supports glob patterns')
+
+parser = argparse.ArgumentParser(description="helper to create qbtthemes")
+parser.add_argument(
+    "-output", type=str, help="output qbtheme file", default="style.qbtheme"
+)
+parser.add_argument("-style", type=str, help="stylesheet", required=True)
+parser.add_argument("-base-dir", type=str, dest="baseDir", default=".")
+parser.add_argument(
+    "-icons-dir",
+    type=str,
+    dest="iconsDir",
+    default="",
+    help="directory which contains custom icons",
+)
+parser.add_argument(
+    "-dir-prefix",
+    type=str,
+    default="",
+    dest="dirPrefix",
+    help="prefix added to all files",
+)
+parser.add_argument(
+    "-config", type=str, dest="config", default=None, help="file used as config.json"
+)
+parser.add_argument(
+    "-find-files",
+    action="store_true",
+    dest="findFiles",
+    help="find files included in qss and only include those",
+)
+parser.add_argument(
+    "files",
+    metavar="files",
+    type=str,
+    nargs="*",
+    default=["*"],
+    help="files to include in resources from baseDir, supports glob patterns",
+)
 
 args = parser.parse_args()
 
-if not args.output.endswith('.qbtheme'):
-    args.output += '.qbtheme'
+if not args.output.endswith(".qbtheme"):
+    args.output += ".qbtheme"
 
 if os.path.exists(args.output):
     print("WARNING! %s already exists. overwriting" % (args.output))
-    
 
-files = allFiles(os.path.join(args.baseDir, '*'))
+
+files = allFiles(os.path.join(args.baseDir, "*"))
 if args.findFiles:
-    print('finding files')
+    print("finding files")
     args.files = []
     stylesheet = open(os.path.join(args.baseDir, args.style)).read()
-    for f in re.findall(':\/uitheme\/(.*)\)', stylesheet):
+    for f in re.findall(":\/uitheme\/(.*)\)", stylesheet):
         args.files.append(f)
-    
+
 config_file = None
 if args.config:
     if os.path.exists(args.config):
@@ -54,28 +83,49 @@ for f in files:
     for i in args.files:
         if fnmatch.fnmatch(alias, i):
             ResourceFiles.append((alias, f))
-            print('adding ' + f)
+            print("adding " + f)
             break
-            
-IconFiles = list() if not args.iconsDir else [f for f in glob.glob(os.path.join(args.iconsDir, '*'))]
+
+IconFiles = (
+    list()
+    if not args.iconsDir
+    else [f for f in glob.glob(os.path.join(args.iconsDir, "*"))]
+)
 print(IconFiles)
 
-with open('resources.qrc', 'w') as rcc:
+with open("resources.qrc", "w") as rcc:
     rcc.write('<!DOCTYPE RCC><RCC version="1.0">\n')
-    rcc.write('\t<qresource %s>\n' % ('prefix=\'' + args.dirPrefix + '\'' if args.dirPrefix else ''))
-    rcc.writelines(['\t\t<file alias=\'%s\'>%s</file>\n' % x for x in ResourceFiles])
-    rcc.write('\t</qresource>\n')
-    rcc.write('\t<qresource>\n')
-    rcc.write('\t\t<file alias=\'stylesheet.qss\'>%s</file>\n' % (os.path.join(args.baseDir, args.style)))
-    rcc.writelines(['\t\t<file alias=\'icons\\%s\'>%s</file>\n' % (os.path.split(x)[1], x) for x in IconFiles])
+    rcc.write(
+        "\t<qresource %s>\n"
+        % ("prefix='" + args.dirPrefix + "'" if args.dirPrefix else "")
+    )
+    rcc.writelines(["\t\t<file alias='%s'>%s</file>\n" % x for x in ResourceFiles])
+    rcc.write("\t</qresource>\n")
+    rcc.write("\t<qresource>\n")
+    rcc.write(
+        "\t\t<file alias='stylesheet.qss'>%s</file>\n"
+        % (os.path.join(args.baseDir, args.style))
+    )
+    rcc.writelines(
+        [
+            "\t\t<file alias='icons\\%s'>%s</file>\n" % (os.path.split(x)[1], x)
+            for x in IconFiles
+        ]
+    )
     if config_file:
-        rcc.write('\t\t<file alias=\'config.json\'>%s</file>\n' % (config_file))
-    rcc.write('\t</qresource>\n')
-    rcc.write('</RCC>')
+        rcc.write("\t\t<file alias='config.json'>%s</file>\n" % (config_file))
+    rcc.write("\t</qresource>\n")
+    rcc.write("</RCC>")
 
 
-cmd = [os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tools/rcc'), '-binary', '-o', args.output, 'resources.qrc']
-print(' '.join(cmd))
+cmd = [
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "tools/rcc"),
+    "-binary",
+    "-o",
+    args.output,
+    "resources.qrc",
+]
+print(" ".join(cmd))
 
 if not subprocess.call(cmd):
-    os.remove('resources.qrc')
+    os.remove("resources.qrc")
