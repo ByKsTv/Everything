f72f7afbeb7d76ade20d22653b0700e51b2bf7a5
ByKsTv <168456084+ByKsTv@users.noreply.github.com>
2026-03-27 16:29:03 +0300

    update

--- Windows/Software/Adobe_Acrobat/Settings.ps1
+++ Windows/Software/Adobe_Acrobat/Settings.ps1
@@ -3,6 +3,7 @@ if (-not (Test-Path -Path 'HKCU:\SOFTWARE\Adobe\Adobe Acrobat\DC\Originals')) {
     New-Item 'HKCU:\SOFTWARE\Adobe\Adobe Acrobat\DC\Originals' -Force
 }
 New-ItemProperty -Path 'HKCU:\SOFTWARE\Adobe\Adobe Acrobat\DC\Originals' -Name 'bDisplayAboutDialog' -Value 0 -PropertyType DWord -Force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown' -Name 'bToggleFTE' -Value 1 -PropertyType DWord -Force
 
 # Adobe Acrobat Pro: Preferences: Catalog: Enable Logging: Off
 if (-not (Test-Path -Path 'HKCU:\SOFTWARE\Adobe\Adobe Acrobat\DC\Catalog\cOptions')) {
